3804022e00af1780b6716ee677786bf0bd6028b0
Alex K <vip-ak47@yandex.ru>
2022-07-03 16:46:32 +0300

    Fixed the shortcuts

--- Game/SceneOptionButton.gd
+++ Game/SceneOptionButton.gd
@@ -10,20 +10,20 @@ func _ready():
 		$KeyLabel.visible = false
 
 func setShortcutPhysicalScancode(code):
-	#var newShortcut = ShortCut.new()
-	#var inputKey = InputEventKey.new()
-	#inputKey.physical_scancode = code
-	#newShortcut.shortcut = inputKey
-	#shortcut = newShortcut
+	var newShortcut = ShortCut.new()
+	var inputKey = InputEventKey.new()
+	inputKey.physical_scancode = code
+	newShortcut.shortcut = inputKey
+	shortcut = newShortcut
 
 	$KeyLabel.text = OS.get_scancode_string(code)
 	savedKey = code
 
-func _unhandled_input(event):
-	if(disabled):
-		return
-	if(event is InputEventKey):
-		if(event.pressed && event.physical_scancode == savedKey):
-			grab_click_focus()
-			grab_focus()
-			emit_signal("pressed")
+#func _unhandled_input(event):
+#	if(disabled):
+#		return
+#	if(event is InputEventKey):
+#		if(event.pressed && event.physical_scancode == savedKey):
+#			grab_click_focus()
+#			grab_focus()
+#			emit_signal("pressed")
