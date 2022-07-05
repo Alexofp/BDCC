0660c1b406f296b3051f1d5eea1374906a70a997
Alex K <vip-ak47@yandex.ru>
2022-07-05 19:19:40 +0300

    Extended Rahi yoga scene

--- Modules/GymModule/RahiYogaScene.gd
+++ Modules/GymModule/RahiYogaScene.gd
@@ -32,6 +32,7 @@ func _run():
 		saynn("[say=rahi]T-thanks.[/say]")
 
 		addButton("Leave", "Enough starring", "leave")
+		addButtonWithChecks("Help", "Ask if the kitty needs some help", "help", [], [[ButtonChecks.StatCheck, Stat.Agility, 10]])
 
 	if(state == "leave"):
 		saynn("You decide not to make the poor kitty look even more embarrassed and stand up from the bench.")
@@ -39,6 +40,71 @@ func _run():
 		# (scene ends)
 		addButton("Continue", "Time to go", "endthescene")
 
+
+	if(state == "help"):
+		# (if agility is high?)
+
+		saynn("You decide to break the awkward silence and move closer to kitty’s mat. You stand near her and slide your hand over her straight back.")
+
+		saynn("[say=pc]Need some help, kitty?[/say]")
+
+		saynn("[say=rahi]Meow..[/say]")
+
+		saynn("[say=pc]C’mon, I have some experience.[/say]")
+
+		saynn("Kitty gradually gets herself back to all fours and then lowers her chest even lower until she is lying on her belly.")
+
+		saynn("[say=rahi]Kitty’s been sitting a lot on the bench and now struggles to do the bow pose..[/say]")
+
+		saynn("[say=pc]Perfect. Show me.[/say]")
+
+		saynn("Kitty mewls again and prepares to try. She arches her spine enough for her torse to be lifted off the ground. Then she bends her knees and tries to reach her feet with her hands. But she clearly struggles, no matter how much she tries, she can’t do it, she lets out some frustrated huffs and eventually gives up.")
+
+		saynn("[say=pc]Alright. Let’s try again with my help.[/say]")
+
+		saynn("Kitty sighs audibly and starts reaching for her feet again. You notice some mistakes in her pose, your hands slide under her exposed girly thighs and make her lift them slightly. Then they spread legs until they are parallel to each other. Kitty lets out a quiet noise but obeys your touch, she tries to hide her blush.")
+
+		saynn("[say=pc]Legs must be lifted off the mat, knees in line with the hips. Ideally only your lower belly and the waist should rest on the mat. Keep your feet engaged.[/say]")
+
+		saynn("Satisfied with that, you move your hands up to her torso and place one hand on her back while the second one gently presses into her chest, just below her C-cup breasts. Kitty squirms slightly under your attention and breathes deeply.")
+
+		saynn("[say=pc]Don’t compress the spine, keep it mostly straight. Instead, use your back muscles to lift your torso up and pull your shoulders back. Try it.[/say]")
+
+		saynn("Kitty, too embarrassed to say anything, follows your advice and begins stretching to try and reach her feet. She draws her shoulder blades back and extends her arms back while trying to keep her torso and legs off the mat. She quickly starts panting and making some kind of noises. You keep your hand just under her breasts, basically cupping them, and gently press, helping the kitty.")
+
+		saynn("Eventually, kitty’s paws manage to reach her feet, her fingers get a grasp on her digi feet. And when she does that, a passionate moan escapes from kitty’s lips, the one that she probably didn’t wanna drop.")
+
+		saynn("[say=rahi]Almost.. A-ah~..[/say]")
+
+		saynn("Her cheeks blush even more, her body tries to squirm from embarrassment but that only causes kitty to let out some more little noises of pleasure.")
+
+		saynn("[say=pc]You seem to like yoga, huh~[/say]")
+
+		saynn("[say=rahi]Yes.. t-thank you..[/say]")
+
+		saynn("You let her keep the pose and stand up. Right now the kitty kinda looks like she is hogtied. But without any ropes to keep her secure, kinda funny.")
+
+		saynn("[say=pc]Ever thought that these inmate clothes are too constraining?[/say]")
+
+		saynn("[say=rahi]Um.. Are they?[/say]")
+
+		saynn("[say=pc]You know, something more skin-tight would be better[/say]")
+
+		saynn("[say=rahi]Kitty doesn’t have anything like that..[/say]")
+
+		saynn("[say=pc]Why not try it naked then?[/say]")
+
+		saynn("[say=rahi]Noooo-.. Others are gonna sta-are.[/say]")
+
+		saynn("Maybe one day.")
+
+		addButton("Leave", "You helped kitty", "leave2")
+
+	if(state == "leave2"):
+		saynn("You offer the feline a headpat as a reward for her efforts. She mewls happily, still holding her pose.")
+
+		addButton("Continue", "Time to go", "endthescene")
+		# (scene ends)
 
 func _react(_action: String, _args):
 	
