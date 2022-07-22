391b373f63841e9cf9465edffb888e6fa63d3718
Alex K <vip-ak47@yandex.ru>
2022-07-22 20:37:11 +0300

    3 new hairstyles

---
+++ Player/Bodyparts/Hair/Ponytail2.gd
@@ -0,0 +1,8 @@
+extends BodypartHair
+
+func _init():
+	visibleName = "ponytail 2"
+	id = "ponytailhair2"
+
+func getDoll3DScene():
+	return "res://Player/Player3D/Parts/Hair/PonytailHair2/PonytailHair2.tscn"
---
+++ Player/Bodyparts/Hair/Tavi.gd
@@ -0,0 +1,8 @@
+extends BodypartHair
+
+func _init():
+	visibleName = "tavi"
+	id = "tavihair"
+
+func getDoll3DScene():
+	return "res://Player/Player3D/Parts/Hair/TaviHair/TaviHair.tscn"
