6c66cb59261046f40da6cfba3e1ca8984b53016b
Alex K <vip-ak47@yandex.ru>
2022-08-01 18:15:31 +0300

    New hairstyle for Tavi

---
+++ Player/Bodyparts/Hair/Overeye.gd
@@ -0,0 +1,8 @@
+extends BodypartHair
+
+func _init():
+	visibleName = "over eye"
+	id = "overeyehair"
+
+func getDoll3DScene():
+	return "res://Player/Player3D/Parts/Hair/OvereyeHair/OvereyeHair.tscn"
