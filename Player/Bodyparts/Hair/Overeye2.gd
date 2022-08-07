4e325db0d79afbeb1c21e9e00f754282742239bc
Alex K <vip-ak47@yandex.ru>
2022-08-07 19:52:12 +0300

    2 more haircuts

---
+++ Player/Bodyparts/Hair/Overeye2.gd
@@ -0,0 +1,8 @@
+extends BodypartHair
+
+func _init():
+	visibleName = "over eye 2"
+	id = "overeyehair2"
+
+func getDoll3DScene():
+	return "res://Player/Player3D/Parts/Hair/OvereyeHair2/OvereyeHair2.tscn"
---
+++ Player/Bodyparts/Hair/Short2.gd
@@ -0,0 +1,8 @@
+extends BodypartHair
+
+func _init():
+	visibleName = "short 2"
+	id = "shorthair2"
+
+func getDoll3DScene():
+	return "res://Player/Player3D/Parts/Hair/ShortHair2/ShortHair2.tscn"
