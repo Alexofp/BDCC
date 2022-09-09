0083f9bff1401198b3e978286f17ba2d2b798550
Alex K <vip-ak47@yandex.ru>
2022-09-09 21:41:00 +0300

    Added some needed flags for the new quest

--- Modules/PortalPantiesModule/Module.gd
+++ Modules/PortalPantiesModule/Module.gd
@@ -3,6 +3,15 @@ extends Module
 func getFlags():
 	return {
 		"Alex_FirstTimeTalked": flag(FlagType.Bool),
+		"Panties_AskedAlex": flag(FlagType.Bool),
+		"Panties_GavePantiesToAlex": flag(FlagType.Bool),
+		"Panties_PcDenied": flag(FlagType.Bool),
+		"Panties_TestsBegan": flag(FlagType.Bool),
+		"Panties_FirstTestHappened": flag(FlagType.Bool),
+		"Panties_TestAmmount": flag(FlagType.Number),
+		"Panties_FleshlightsGotStolen": flag(FlagType.Bool),
+		"Panties_FleshlightsAskedAlex": flag(FlagType.Bool),
+		"Panties_FleshlightsReturnedToAlex": flag(FlagType.Bool),
 	}
 
 func _init():
