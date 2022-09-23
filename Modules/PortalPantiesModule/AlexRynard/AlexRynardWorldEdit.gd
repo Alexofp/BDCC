a0f0f84a43155d386e3b348e1e3a797aaf65a704
Alex K <vip-ak47@yandex.ru>
2022-09-23 23:46:41 +0300

    Fixed clothing clipping with bras with big breasts

--- Modules/PortalPantiesModule/AlexRynard/AlexRynardWorldEdit.gd
+++ Modules/PortalPantiesModule/AlexRynard/AlexRynardWorldEdit.gd
@@ -4,6 +4,5 @@ func _init():
 	id = "AlexRynardWorldEdit"
 	
 func apply(world: GameWorld):
-	print("MEW ", world)
 	if(getFlag("PortalPantiesModule.Alex_FirstTimeTalked")):
 		world.setRoomSprite("eng_workshop", RoomStuff.RoomSprite.PERSON)
