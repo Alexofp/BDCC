3a11eeeb399c01e1c6455d33e36118b7d6d181dd
Alex K <vip-ak47@yandex.ru>
2023-02-01 17:39:08 +0300

    Next story scene

--- Modules/TaviModule/Module.gd
+++ Modules/TaviModule/Module.gd
@@ -106,6 +106,7 @@ func _init():
 		"res://Modules/TaviModule/Chapter3/Ch3s5CaptainScene.gd",
 		"res://Modules/TaviModule/Chapter4/Ch4s1SolitaryScene.gd",
 		"res://Modules/TaviModule/Chapter4/Ch4s2TaviFlashbackIntroScene.gd",
+		"res://Modules/TaviModule/Chapter4/Ch4s3RishaFightScene.gd",
 		]
 	characters = [
 		"res://Modules/TaviModule/Chapter4/DirectorTau.gd",
