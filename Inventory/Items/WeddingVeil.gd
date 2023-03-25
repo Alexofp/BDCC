9960a4513bd7b267e4b76032c6d1b74a680503da
Rahi <vip-ak47@yandex.ru>
2023-03-25 14:42:40 +0300

    Wedding veil

---
+++ Inventory/Items/WeddingVeil.gd
@@ -0,0 +1,40 @@
+extends ItemBase
+
+func _init():
+	id = "WeddingVeil"
+
+func getVisibleName():
+	return "Wedding veil"
+	
+func getDescription():
+	return "A fancy veil that you wear during weddings"
+
+func getClothingSlot():
+	return InventorySlot.Eyes
+
+func getBuffs():
+	return [
+		]
+
+func getTags():
+	return [
+		]
+
+func getTakingOffStringLong(withS):
+	if(withS):
+		return "takes off your veil"
+	else:
+		return "take off your veil"
+
+func getPuttingOnStringLong(withS):
+	if(withS):
+		return "puts on your veil"
+	else:
+		return "put on your veil"
+
+func getRiggedParts(_character):
+	if(itemState.isRemoved()):
+		return null
+	return {
+		"veil": "res://Inventory/RiggedModels/WeddingVeil/WeddingVeil.tscn",
+	}
