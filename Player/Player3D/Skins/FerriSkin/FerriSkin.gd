71e5bd3bb6db7a2996a1fdc96f8fb03ade228de1
Rahi <vip-ak47@yandex.ru>
2023-06-24 20:41:57 +0300

    2 more skins

---
+++ Player/Player3D/Skins/FerriSkin/FerriSkin.gd
@@ -0,0 +1,10 @@
+extends SkinBase
+
+func _init():
+	id = "FerriSkin"
+
+func getName():
+	return "Ferocious"
+
+func getPatternTexture():
+	return preload("res://Player/Player3D/Skins/FerriSkin/ferriSkin.png")
---
+++ Player/Player3D/Skins/WoonaSkin/WoonaSkin.gd
@@ -0,0 +1,10 @@
+extends SkinBase
+
+func _init():
+	id = "WoonaSkin"
+
+func getName():
+	return "Anger"
+
+func getPatternTexture():
+	return preload("res://Player/Player3D/Skins/WoonaSkin/woonaSkin.png")
