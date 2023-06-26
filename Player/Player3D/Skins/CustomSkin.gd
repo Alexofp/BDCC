58103a908c24f84dfae4a7ae9e148efe6e6ddc12
Rahi <vip-ak47@yandex.ru>
2023-06-26 20:39:23 +0300

    demon tail pattern, custom skins can be used by npcs

--- Player/Player3D/Skins/CustomSkin.gd
+++ Player/Player3D/Skins/CustomSkin.gd
@@ -3,6 +3,7 @@ extends SkinBase
 var customName = ""
 var customTexture
 var texturePath
+var calculatedFitting = {}
 
 func setTexturePath(newPath):
 	customTexture = ImageTexture.new()
@@ -15,9 +16,22 @@ func setTexturePath(newPath):
 		return
 	
 	customTexture.create_from_image(image, 4)
+	
+	calculatedFitting = {}
+	if("_f" in customName):
+		calculatedFitting[SkinType.Fur] = 1.0
+	if("_s" in customName):
+		calculatedFitting[SkinType.Scales] = 1.0
+	if("_h" in customName):
+		calculatedFitting[SkinType.Skin] = 1.0
+	if("_m" in customName):
+		calculatedFitting[SkinType.SkinAndFur] = 1.0
 
 func getName():
 	return customName
 
 func getPatternTexture():
 	return customTexture
+
+func getFittingSkinTypes():
+	return calculatedFitting
