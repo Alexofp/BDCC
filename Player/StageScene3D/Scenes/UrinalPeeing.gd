b39a610de88f00148bf0796b9f7d44b063ac6e09
Rahi <vip-ak47@yandex.ru>
2023-10-19 19:25:22 +0300

    New animations implemented in code

---
+++ Player/StageScene3D/Scenes/UrinalPeeing.gd
@@ -0,0 +1,87 @@
+extends BaseStageScene3D
+
+onready var animationTree = $AnimationTree
+onready var animationTree2 = $AnimationTree2
+onready var doll = $Doll3D
+onready var doll2 = $Doll3D2
+
+func _init():
+	id = StageScene.UrinalPeeing
+
+func _ready():
+	animationTree.anim_player = animationTree.get_path_to(doll.getAnimPlayer())
+	animationTree.active = true
+	animationTree2.anim_player = animationTree2.get_path_to(doll2.getAnimPlayer())
+	animationTree2.active = true
+	
+func updateSubAnims():
+	if(true):
+		return
+	if(doll.getArmsCuffed()):
+		animationTree["parameters/CuffsBlend/blend_amount"] = 1.0
+	else:
+		animationTree["parameters/CuffsBlend/blend_amount"] = 0.0
+	
+	if(doll2.getArmsCuffed()):
+		animationTree2["parameters/CuffsBlend/blend_amount"] = 1.0
+	else:
+		animationTree2["parameters/CuffsBlend/blend_amount"] = 0.0
+
+func playAnimation(animID, _args = {}):
+	var firstDoll = "pc"
+	if(_args.has("pc")):
+		firstDoll = _args["pc"]
+	doll.prepareCharacter(firstDoll)
+	var secondDoll = "pc"
+	if(_args.has("npc")):
+		secondDoll = _args["npc"]
+	doll2.prepareCharacter(secondDoll)
+	
+	if(_args.has("bodyState")):
+		doll.applyBodyState(_args["bodyState"])
+	else:
+		doll.applyBodyState({})
+	
+	if(_args.has("npcBodyState")):
+		doll2.applyBodyState(_args["npcBodyState"])
+	else:
+		doll2.applyBodyState({})
+	
+	updateSubAnims()
+	
+	var state_machine = animationTree["parameters/StateMachine/playback"]
+	var state_machine2 = animationTree2["parameters/StateMachine/playback"]
+
+	var pcAnim = "idle2"
+	if(_args.has("pcAction")):
+		pcAnim = _args["pcAction"]
+	
+	if(pcAnim == "idle2"):
+		state_machine.travel("UrinalIdle2-loop")
+	elif(pcAnim == "idle"):
+		state_machine.travel("UrinalIdle-loop")
+
+	if(animID == "idle"):
+		state_machine2.travel("UrinalStrokingIdle_2-loop")
+	if(animID == "stroke"):
+		state_machine2.travel("UrinalStroking_2-loop")
+	if(animID == "pee"):
+		state_machine2.travel("UrinalPeeing_2-loop")
+	if(animID == "peefemale"):
+		state_machine2.travel("UrinalPeeingFemale_2-loop")
+
+
+func canTransitionTo(_actionID, _args = []):
+	var firstDoll = "pc"
+	if(_args.has("pc")):
+		firstDoll = _args["pc"]
+	var secondDoll = "pc"
+	if(_args.has("npc")):
+		secondDoll = _args["npc"]
+		
+	if(doll.getCharacterID() != firstDoll || doll2.getCharacterID() != secondDoll):
+		return false
+	return true
+
+func getSupportedStates():
+	return ["idle", "stroke", "pee", "peefemale"]
