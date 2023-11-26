2d9b0ca0fec289a8b125e3a1bb13b2e1f1adaa4e
Rahi <vip-ak47@yandex.ru>
2023-11-26 18:12:44 +0300

    More work on Alex Trust Scene

--- Modules/AlexRynardModule/Trust/AlexTrustExercise1Scene.gd
+++ Modules/AlexRynardModule/Trust/AlexTrustExercise1Scene.gd
@@ -1,5 +1,7 @@
 extends SceneBase
 
+var bratCounter = 0
+
 func _init():
 	sceneID = "AlexTrustExercise1Scene"
 
@@ -203,8 +205,8 @@ func _run():
 
 		saynn("Seems doable..")
 
-		addButton("Stay still", "Do as he orders", "2_still")
 		addButton("Move slightly", "Wiggle your butt slightly while he isn't watching", "2_brat")
+		addButton("Stay still", "Do as he orders", "2_still")
 		addButton("RED", "Say the safeword", "2_red")
 	if(state == "2_red"):
 		saynn("[say=pc]Red.[/say]")
@@ -235,6 +237,229 @@ func _run():
 		saynn("He brings you back into the workshop.")
 
 		addButton("Continue", "See what happens next", "endthescene")
+	if(state == "2_still"):
+		saynn("You do your best, freezing in place. Your hands are placed by your sides, your gaze directed exactly forward. No words leave your mouth.")
+
+		saynn("You still see Alex in the corner of your vision. He scratches his chin slightly.")
+
+		saynn("[say=alexrynard]Good. Let's see what we're working with.[/say]")
+
+		addButton("Continue", "See what happens next", "3")
+	if(state == "2_brat"):
+		saynn("You freeze in place, your hands are placed by your sides, your gaze directed exactly forward. You still see Alex in the corner of your vision.. and that makes you wonder about how much you can get away with.")
+
+		saynn("Alex scratches his chin slightly while you proceed to wiggle your {pc.masc} behind ever so slightly, just swaying it, following the imaginary wind.")
+
+		saynn("Hah. He doesn't notice. Makes you smile softly as you taunt him more with your butt.")
+
+		saynn("[say=alexrynard]Hm..[/say]")
+
+		saynn("You freeze again when he approaches you. His gaze is drilling you..")
+
+		saynn("Suddenly, he raises his hand.. and quickly brings it down on your butt.. before stopping at the last possible moment, avoiding spanking you.")
+
+		saynn("[say=alexrynard]Next one is gonna be real.[/say]")
+
+		saynn("Fair enough.. You are pouting a bit while standing still.")
+
+		saynn("[say=alexrynard]Let's see what we're working with.[/say]")
+
+		addButton("Continue", "See what happens next", "3")
+	if(state == "3"):
+		saynn("He stands in front of you. Arms crossed. Gaze focussed.")
+
+		saynn("You can hear subtle humming as he examines your face features. Then, his palm boldly reaches out to cup your chin, making you raise your head a little.")
+
+		saynn("[say=alexrynard]Mhm. Hm.[/say]")
+
+		saynn("After checking out your face in great detail, he moves on to checking your {pc.masc} body too. His palm boldly travels over the curve of your shoulder, sending shivers through your muscles.. before slipping off.")
+
+		saynn("Alex starts circling you, going out of your view, checking you out from the behind too. You don't dare to turn your head.. so you have no idea what he is doing behind you. Only his silent steps echo throughout the room.. along with your increased heart rate.")
+
+		saynn("You can't shake off this thought.. Are you being judged? No, that's not it.. He said that you are in control here. And yet, right now you feel so..")
+
+		saynn("Alex ends your train of thought early by taking a spot in front of you again.")
+
+		saynn("[say=alexrynard]Pretty cute. I want you to forget that you are an inmate for now. You don't belong to this prison.[/say]")
+
+		saynn("He cups your chin again, his other palm brushes over your cheek. While you are..")
+
+		addButton("Allow it", "Let him do what he is doing", "3_allow")
+		addButton("Stick tongue out", "", "3_brat")
+		addButton("RED", "Say this", "3_red")
+	if(state == "3_red"):
+		saynn("..not having any of this.")
+
+		saynn("[say=pc]Red.[/say]")
+
+		saynn("Alex swiftly pulls his hands away.")
+
+		saynn("[say=alexrynard]Did I make you uncomfortable?[/say]")
+
+		saynn("He is clearly a bit sad. But his voice doesn't sound so.")
+
+		saynn("[say=pc]A little bit, yes.[/say]")
+
+		saynn("[say=alexrynard]My bad. Well, you got the taste of it at least. Want a hug maybe?[/say]")
+
+		saynn("[say=pc]A hug?[/say]")
+
+		saynn("[say=alexrynard]Just a friendly hug.[/say]")
+
+		addButton("Get hugged", "Huggies!", "3_red_hug")
+		addButton("It's fine", "No hug is fine", "3_red_nohug")
+	if(state == "3_red_hug"):
+		playAnimation(StageScene.Hug, "hug", {pc="alexrynard", npc="pc"})
+		saynn("Might as well let him hug you..")
+
+		saynn("You nod.. and then feel his warm embrace.")
+
+		saynn("Feels nice..")
+
+		saynn("After that little gesture, he helps you get back to the workshop.")
+
+		addButton("Continue", "See what happens next", "endthescene")
+	if(state == "3_red_nohug"):
+		saynn("[say=pc]I'm good.[/say]")
+
+		saynn("Alex shrugs.")
+
+		saynn("[say=alexrynard]Well, let's go back then.[/say]")
+
+		saynn("He helps you get back to the workshop.")
+
+		addButton("Continue", "See what happens next", "endthescene")
+	if(state == "3_brat"):
+		saynn("..boldly sticking your tongue out at him. Alex raises a brow at you while you can't help but to smile.")
+
+		saynn("Your smug attitude goes away very soon though as Alex suddenly catches your tongue with his claws, making it so you can't pull it back.")
+
+		saynn("[say=pc]Oww-wh..[/say]")
+
+		saynn("[say=alexrynard]You're free to brat, brat. As long as you are ready for the consequences.[/say]")
+
+		saynn("You'd maybe say something but that's hard when you can't control your wet tongue. Your cheeks turn slightly red as you start drooling.")
+
+		saynn("[say=alexrynard]Can I let go now?[/say]")
+
+		saynn("You nod softly. And so Alex pulls his claws away from your wet tongue.")
+
+		saynn("[say=alexrynard]Adorable.[/say]")
+
+		saynn("He is smiling.")
+
+		saynn("[say=alexrynard]Forget that you belong to this prison. During this very.. scene.. you belong to me and me only.[/say]")
+
+		addButton("Continue", "See what happens next", "4")
+	if(state == "3_allow"):
+		saynn("..allowing him to do it, standing completely still, just like you were ordered. His touch is warm anyway, why would you deny it.")
+
+		saynn("[say=alexrynard]For the duration of this very.. scene.. you belong to me now. And me only.[/say]")
+
+		addButton("Continue", "See what happens next", "4")
+	if(state == "4"):
+		saynn("Alex leans in real close.. almost like he is about to kiss you.. just to take a step back moments later.")
+
+		saynn("[say=alexrynard]If you understood it.. I want you to start addressing me as Sir from now on. Yes, Sir. With that exact intonation.[/say]")
+
+		addButton("Yes, sir", "Say this", "4_brat")
+		addButton("Yes, Sir", "Say this", "4_yessir")
+		addButton("RED", "Say this", "4_red")
+		if (false):
+			addButton("Test", "You shouldn't see this", "4_brat")
+	if(state == "4_red"):
+		saynn("[say=pc]RED.[/say]")
+
+		saynn("Alex puts away his drilling gaze.")
+
+		saynn("[say=alexrynard]Too much for you?[/say]")
+
+		saynn("You nod. Alex lowers his gaze.")
+
+		saynn("[say=alexrynard]I understand. Want a hug?[/say]")
+
+		saynn("[say=pc]A hug?[/say]")
+
+		saynn("He nods.")
+
+		addButton("Get hugged", "Huggies!", "3_red_hug")
+		addButton("It's fine", "No hug is fine", "3_red_nohug")
+	if(state == "4_yessir"):
+		saynn("Short and snappy, shouldn't be too hard to remember..")
+
+		saynn("[say=pc]Yes, Sir![/say]")
+
+		saynn("You are still looking forward while exclaiming that.")
+
+		saynn("Corners of Alex's mouth move up a bit. But then he catches himself, his face switching back to a neutral expression.")
+
+		saynn("[say=alexrynard]Yes what?[/say]")
+
+		saynn("[say=pc]I belong to you.. in this scene.. Sir.[/say]")
+
+		saynn("He nods a few times softly.")
+
+		addButton("Continue", "See what happens next", "5")
+	if(state == "4_brat"):
+		saynn("What was the assignment again?.. Oh right.")
+
+		saynn("[say=pc]Yes, sir.[/say]")
+
+		saynn("You stand still, puffing your chest out proudly, awaiting your reward.")
+
+		saynn("[say=alexrynard]Try again.[/say]")
+
+		saynn("You put on confused eyes.")
+
+		saynn("[say=pc]Huh? But that's what you asked![/say]")
+
+		saynn("[say=alexrynard]Is that what I asked?[/say]")
+
+		saynn("[say=pc]Yes?[/say]")
+
+		saynn("[say=alexrynard]Repeat after me.[/say]")
+
+		saynn("So annoying!")
+
+		saynn("[say=alexrynard]Yes.[/say]")
+
+		saynn("[say=pc]Yes.[/say]")
+
+		saynn("[say=alexrynard]Sir.[/say]")
+
+		saynn("[say=pc]Sir.[/say]")
+
+		saynn("[say=alexrynard]Yes, Sir.[/say]")
+
+		saynn("[say=pc]Yes, sir.[/say]")
+
+		saynn("Alex blinks many times. And then starts glaring. You bravely stare back. But that's when his hand suddenly smacks your bratty butt!")
+
+		saynn("[say=pc]OW![/say]")
+
+		saynn("[say=alexrynard]Ow who?[/say]")
+
+		saynn("[say=pc]OW, SIR![/say]")
+
+		saynn("[say=alexrynard]That's the enthusiasm I'm looking for. Knew you had it in you.[/say]")
+
+		saynn("You pout.. while Alex smirks.")
+
+		addButton("Continue", "See what happens next", "5")
+	if(state == "5"):
+		saynn("Alex is still crossing his arms, his stare still making you feel smaller than you are. And yet, you feel comfortable..")
+
+		saynn("[say=alexrynard]I'd like to see what I have.[/say]")
+
+		saynn("His eyes take note of your clothes.")
+
+		saynn("[say=alexrynard]Fully.[/say]")
+
+		addButton("Undress", "Start taking off your clothing for him", "5_undress")
+		addButton("Turn around", "He clearly wants to see you from every angle!", "5_turnaround")
+		addButton("RED", "Say this", "5_red")
+	if(state == "5_undress"):
+		playAnimation(StageScene.Duo, "stand", {npc="alexrynard", bodyState={naked=true}})
 
 func _react(_action: String, _args):
 	if(_action == "endthescene"):
@@ -244,7 +469,45 @@ func _react(_action: String, _args):
 	if(_action == "brought_intro_breakroom"):
 		processTime(3*60)
 
+	if(_action == "first_green"):
+		bratCounter += 1
+
+	if(_action == "first_red_no"):
+		bratCounter += 1
+
+	if(_action == "1_brat"):
+		bratCounter += 1
+
 	if(_action == "2"):
 		processTime(5*60)
 
+	if(_action == "2_brat"):
+		bratCounter += 1
+
+	if(_action == "3_brat"):
+		bratCounter += 1
+
+	if(_action == "3_red_hug"):
+		processTime(3*60)
+		GM.pc.addPain(-20)
+		addMessage("Hugs are nice!")
+
+	if(_action == "4_brat"):
+		bratCounter += 1
+
+	if(_action == "5_turnaround"):
+		bratCounter += 1
+
 	setState(_action)
+
+func saveData():
+	var data = .saveData()
+
+	data["bratCounter"] = bratCounter
+
+	return data
+
+func loadData(data):
+	.loadData(data)
+
+	bratCounter = SAVE.loadVar(data, "bratCounter", 0)
