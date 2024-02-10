e7ea19ab09c5e856ca8bc9eab8db65a3bf61fcff
Rahi <vip-ak47@yandex.ru>
2024-02-10 19:27:26 +0300

    More work on Socket's 4th scene

--- Modules/SocketModule/HelpScenes/SocketHelp4Scene.gd
+++ Modules/SocketModule/HelpScenes/SocketHelp4Scene.gd
@@ -109,7 +109,196 @@ func _run():
 	if(state == "bulldog_talk"):
 		addCharacter("bulldog")
 		playAnimation(StageScene.Duo, "stand", {pc="socket", npc="bulldog"})
-		saynn("You and Socket approach him.")
+		saynn("You and Socket approach the guy that is blocking some maintenance hatch with his body. He reacts to you two by crossing his arms and putting on a very mean face. The maintenance door that he is protecting.. is way bigger this time.")
+
+		saynn("[say=socket]Hi![/say]")
+
+		saynn("No response. The guy just quickly eyes Socket's exposed bits and returns to being still.")
+
+		saynn("[say=socket]I need to check this maintenance vent, can you please stand aside?[/say]")
+
+		saynn("She is so nice.. but the guy just shakes his head.")
+
+		saynn("[say=socket]Just move a little, I don't need much space![/say]")
+
+		saynn("You know for a fact that her words are not correct.")
+
+		if (hasAccess):
+			saynn("It's your turn to try.")
+
+			saynn("[say=pc]C'mon, you know me. Just let us past.[/say]")
+
+			saynn("The bulldog stares you deeply into the eyes. She points at you first.")
+
+			saynn("[say=bulldog]You can pass.[/say]")
+
+			saynn("Then he points at Socket.")
+
+			saynn("[say=bulldog]You can't.[/say]")
+
+			saynn("[say=socket]Why not?[/say]")
+
+			saynn("No answer.")
+
+		else:
+			saynn("It's your turn to try.")
+
+			saynn("[say=pc]Staff is telling you to move. I think you should move. Otherwise, you know, who knows what will happen.[/say]")
+
+			saynn("The bulldog stares you deeply into the eyes.")
+
+			saynn("[say=bulldog]Uh huh.[/say]")
+
+		saynn("You shrug and turn towards Socket.")
+
+		saynn("[say=pc]Maybe you can let someone else do this one?[/say]")
+
+		saynn("The fennec thinks about it for a second.")
+
+		saynn("[say=socket]But now I'm even more curious to see what's behind![/say]")
+
+		saynn("She walks up closer to the guy.. causing him to tense up. Watching Socket made something else of him tense up too.")
+
+		saynn("[say=socket]C'mon, big guy! I see that you are horny! Wanna tell us what you want and we can try to meet you half-way?[/say]")
+
+		saynn("She winks to him.")
+
+		saynn("[say=bulldog]Fuck off while you still have all the bones in tact.[/say]")
+
+		saynn("[say=socket]That wasn't nice![/say]")
+
+		saynn("Suddenly, Socket whips her giant wrench out and swings it, aiming for his balls.. but stopping just fast enough for the metal tip to tap on his bulge.")
+
+		saynn("[say=socket]Bonk?[/say]")
+
+		saynn("His eyes.. go wide. Even though his crotch wasn't hit, he still grabs onto it, losing any kind of.. composure.. in the process.")
+
+		saynn("[say=bulldog]What the fuck, woman?[/say]")
+
+		saynn("[say=socket]What? You were talking about bones, not me! Also, you're being rude again![/say]")
+
+		saynn("Socket prepares to swing again.. but the guy is quick to step away.")
+
+		saynn("[say=bulldog]Whatever, fuck off.[/say]")
+
+		saynn("[say=socket]Yay, good inmate, thank you.[/say]")
+
+		saynn("[say=bulldog]Yeah, good luck leaving it.[/say]")
+
+		saynn("Socket smiles.")
+
+		saynn("[say=socket]I already got stuck once in these, {pc.name} here will help me if I do![/say]")
+
+		if (hasAccess):
+			saynn("You scratch behind your head. You still think that this might not be such a good idea.")
+
+			saynn("[say=pc]You really shouldn't go there, trust me.[/say]")
+
+			saynn("[say=socket]Why not? I have a task and I must complete it![/say]")
+
+			saynn("Well, maybe it will be fine.")
+
+		else:
+			saynn("You scratch behind your head. You're not sure how good of an idea this is.")
+
+			saynn("[say=pc]You're sure we should go there?[/say]")
+
+			saynn("[say=socket]Why not? I have a task and I must complete it![/say]")
+
+			saynn("Well, maybe it will be fine.")
+
+		addButton("Follow", "See what happens next", "follow_socket_underground")
+	if(state == "follow_socket_underground"):
+		aimCameraAndSetLocName("fight_entrance")
+		playAnimation(StageScene.Duo, "walk", {npc="socket", flipNPC=true, npcAction="walk"})
+		saynn("Socket bonks the hatch.. making it open, revealing a deep dark corridor that is heading somewhere.")
+
+		saynn("[say=socket]Oh wow, an adventure.[/say]")
+
+		saynn("She starts following it.. while you follow behind. The hatch behind closes, putting both of you into darkness..")
+
+		saynn("[say=socket]Tell me if you see a leaking pipe![/say]")
+
+		saynn("[say=pc]Hard to see anything really.[/say]")
+
+		saynn("It was hard to miss luckily, a loud hiss guided you to a pipe that had a gaping hole in it.")
+
+		saynn("[say=socket]There it is![/say]")
+
+		saynn("Socket grabs her wrench and starts tightening the pipe before that one, closing it up and making the pressure in it go down.. until the hot gas has stopped escaping it.")
+
+		saynn("[say=socket]Can you hold it please?[/say]")
+
+		saynn("You hold the wrench while Socket replaces the pipe with a new shiny one.")
+
+		saynn("[say=socket]Thanks![/say]")
+
+		saynn("She grabs the wrench back and opens the pipe again.")
+
+		saynn("[say=socket]Yay, no leak. Time to cross it off the list.[/say]")
+
+		saynn("Socket grabs her datapad and marks the task as completed. Huh, for some reason the hiss didn't go away fully though, you both hear it.")
+
+		saynn("[say=socket]Wait. There is probably another leak.[/say]")
+
+		saynn("And so you continue following the dark narrow maintenance corridor that looks very abandoned. The hiss gets louder.. but it also changes pitch. Sounds like.. cheering..")
+
+		addButton("Continue", "See what happens next", "socket_in_underground")
+	if(state == "socket_in_underground"):
+		aimCameraAndSetLocName("fight_nearentrance")
+		playAnimation(StageScene.Duo, "stand", {npc="socket"})
+		saynn("The corridor ends.. revealing a big open space. It's dimly lit.. but it's easy to see a crowd of inmates standing around some kind of cage.. arena.")
+
+		saynn("[say=socket]Wow.. This was happening underneath the station all this time?[/say]")
+
+		saynn("There is some kind of fight happening so everyone's attention is diverted to it, inmates are cheering loudly after each punch and kick.")
+
+		saynn("[say=socket]That is.. so cool! Should we go look at the fights?[/say]")
+
+		saynn("[say=pc]I don't think the staff members are exactly welcome here. I think we should leave while we..[/say]")
+
+		saynn("[say=announcer]Hey there, sweethearts.[/say]")
+
+		addButton("Continue", "See what happens next", "announcer_talk")
+	if(state == "announcer_talk"):
+		addCharacter("announcer")
+		playAnimation(StageScene.Duo, "stand", {pc="socket", npc="announcer"})
+		aimCameraAndSetLocName("fight_neararena")
+		if (hasAccess):
+			saynn("You turn and see Ans, the guy who runs this place. He is holding a cane, just like always.")
+
+		else:
+			saynn("You turn and see a lilac inmate. He is holding a cane which makes him look.. important.")
+
+		saynn("[say=socket]Hi! You look fancy.[/say]")
+
+		saynn("[say=announcer]Thanks, baby girl. Hah. Your appearance is nothing short of a miracle too. Not very often do we get staffers here.[/say]")
+
+		saynn("[say=socket]Oh, should I leave?[/say]")
+
+		saynn("He shrugs and spreads his arms wide.")
+
+		saynn("[say=announcer]Somehow you got here. And so, you're welcome to stay in my little paradice. What I don't want is.. other eyes and mouths. And stun batons.[/say]")
+
+		saynn("What a kind guy.")
+
+		saynn("[say=socket]Ohh, you want me to be hush-hush about this place.[/say]")
+
+		saynn("He walks up closer and pats Socket on the back.")
+
+		saynn("[say=announcer]Exactly. You're a smart bean. Would be a shame to see my little secret place gone, all our visitors like it. Just look at them.[/say]")
+
+		saynn("The crowd cheers as one of the opponents finally hits the floor.")
+
+		saynn("[say=avy]Hey, the fuck is this?[/say]")
+
+		addButton("Continue", "See what happens next", "avy_appears")
+	if(state == "avy_appears"):
+		addCharacter("avy")
+		playAnimation(StageScene.Duo, "stand", {pc="socket", npc="avy"})
+		saynn("[say=avy]Wow, you're even more of an idiot than I thought.[/say]")
+
+		saynn("[sayOther]Pausing user's thought process.. Success. Keep in mind that overstimulation of the wearer's senses while in a paused state might lead to short-term memory loss or corruption. Current chance of memory loss is 0%.[/sayOther]")
 
 
 func _react(_action: String, _args):
@@ -125,6 +314,18 @@ func _react(_action: String, _args):
 
 	if(_action == "bulldog_talk"):
 		processTime(5*60)
+
+	if(_action == "follow_socket_underground"):
+		processTime(5*60)
+
+	if(_action == "socket_in_underground"):
+		processTime(5*60)
+
+	if(_action == "announcer_talk"):
+		processTime(3*60)
+
+	if(_action == "avy_appears"):
+		processTime(3*60)
 
 	setState(_action)
 
