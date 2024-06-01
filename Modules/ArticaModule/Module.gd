82bccf542212ba345ab050a32c21cef93675e8bf
Rahi <vip-ak47@yandex.ru>
2024-06-01 18:29:47 +0300

    Began work on the last corruption event

--- Modules/ArticaModule/Module.gd
+++ Modules/ArticaModule/Module.gd
@@ -92,6 +92,8 @@ func _init():
 		"res://Modules/ArticaModule/c1Corruption/Event/articaEventDildo1Scene.gd",
 		"res://Modules/ArticaModule/c1Corruption/Event/articaEventDildo2Scene.gd",
 		"res://Modules/ArticaModule/c1Corruption/Event/articaEventDildo3Scene.gd",
+		
+		"res://Modules/ArticaModule/c1Corruption/Event/articaEventPortalPanties1Scene.gd",
 		]
 	characters = [
 		"res://Modules/ArticaModule/ArticaCharacter.gd",
