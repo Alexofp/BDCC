4bfb7161251ddf5816bcfa6174a0188f48b34a01
CanInBad <73659199+CanInBad@users.noreply.github.com>
2024-09-21 15:01:17 +0700

    Added cached side and fixed inversion

--- Player/Player3D/Parts/PartOrientPicker.gd
+++ Player/Player3D/Parts/PartOrientPicker.gd
@@ -6,6 +6,9 @@ export(bool) var invert = false
 var _theDoll:Spatial
 # stores the doll itself by sarching in loop
 
+var _cachedSide:bool = false
+# we can get into the loop if the actual side compared to this is changed
+
 func _ready():
 	var Part3D = load("res://Player/Player3D/Parts/Part3D.gd")
 	var theNode:Node = self # I really hope this works
@@ -17,13 +20,25 @@ func _ready():
 			_theDoll = previousNode.getDoll()
 		theNode = previousNode
 
+	var _currentSide = (_theDoll.get_scale().x == -1)
+	if invert: # yes this is boilerplate so what
+		setValue(!_currentSide)
+	else:
+		setValue(_currentSide)
+
+	_cachedSide = _currentSide # set init "cached" side
+
 func _process(_delta):
 	if _theDoll == null:
 		return # i sure hope running this code every single frame doesn't add up.
-	if invert:
-		setValue(_theDoll.get_scale().x == -1)
-	else:
-		setValue(!(_theDoll.get_scale().x == -1))
+
+	var _currentSide = (_theDoll.get_scale().x == -1)
+	if _cachedSide != _currentSide: # check if current side changes
+		_cachedSide = _currentSide
+		if invert: # basically the same as L24-L27
+			setValue(!_currentSide)
+		else:
+			setValue(_currentSide)
 
 func setValue(value):
 	for child in get_children():
