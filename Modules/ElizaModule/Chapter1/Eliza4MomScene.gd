ec7446054990ac38532211e863d5717d606aaafc
Rahi <vip-ak47@yandex.ru>
2025-01-29 19:10:37 +0300

    Finished a story scene

--- Modules/ElizaModule/Chapter1/Eliza4MomScene.gd
+++ Modules/ElizaModule/Chapter1/Eliza4MomScene.gd
@@ -8,7 +8,7 @@ func _run():
 		playAnimation(StageScene.Solo, "stand")
 		saynn("[say=eliza]Really?![/say]")
 
-		saynn("Your current journey is interrupted by Quinn's loud voice. Something is happening here, in the lobby..")
+		saynn("Your casual stride through the lobby is abruptly interrupted by a raised voice.. Eliza's voice.")
 
 		addButton("Take a look", "See what's up", "mom_intro")
 	if(state == "mom_intro"):
@@ -16,104 +16,136 @@ func _run():
 		addCharacter("eliza")
 		playAnimation(StageScene.Duo, "stand", {pc="eliza", npc="elizaMom"})
 		aimCameraAndSetLocName("med_lobbymain")
-		saynn("Eliza is standing behind the counter, talking with another woman.")
+		saynn("Eliza's feline ears twitch, her tail wagging behind her as she leans against one of the walls behind the counter, staring wide-eyed at the woman across from her. You only heard a single word so far.. but it's enough to raise the tension high.")
 
-		saynn("[say=elizaMom]I'm starting to think that this was a mistake on my part.[/say]")
+		saynn("[say=elizaMom]I'm starting to think that this was a mistake.[/say]")
+
+		saynn("A slightly raspy voice, a streaks of dignified silver in her fur and hair, official clothes.. the woman near Eliza is radiating sheer levels of authority that makes even the nurses try to stay away. Near her are two guards.. ones that you don't recognize.")
 
 		saynn("[say=eliza]But Mom![/say]")
 
-		saynn("Oh.. It makes sense now, why the second woman reminds you a lot of Eliza. She is a feline.. clearly in her fifties.. which is evident by the occasional prestigious gray stripes in her hair.")
+		saynn("Oh.. some things click into place now. Now that you know it, the woman does remind you of Eliza quite a bit.. just with more.. experience behind her back.")
 
-		saynn("[say=elizaMom]I didn't send you here to make drugs, sweetie.[/say]")
+		saynn("[say=elizaMom]I didn't send you here to make drugs, sweetie. Lightly speaking.[/say]")
 
-		saynn("There is a hint of a very strong but suppressed feeling in Eliza's mother's gaze. It slowly becomes more obvious as she looks around.")
+		saynn("There is no venom in her voice.. but there is no warmth either. Her sharp eyes scan the lobby, gaze lingering on the cluttered workspace behind the counter.. a laptop, all sorts of lab notes that all look like they have scribbles of a mad person on them, some kind of tools.. and also a mug of coffee with the words 'Best Mom' written on the side.")
 
-		saynn("[say=eliza]But that is part of.. part of everything that we do. That's what I wanna do! Not drugs, I mean.. but you know.. Why can't you be like dad?[/say]")
+		saynn("Eliza stiffens.")
 
-		saynn("[say=elizaMom]Your father is a fool. He would happily waste money on new equipment. Equipment that would become ash in mere weeks, as you and I learned by now.[/say]")
+		saynn("[say=eliza]That's not.. I mean.. I'm not just making drugs. I'm a chemist, I'm studying them. That's what I wanna do! Why can't you be like dad?[/say]")
+
+		saynn("[say=elizaMom]Your father is a fool, bless his reckless heart. He would throw money at your projects without a second thought.. and then watch it all go up in flames. Like our laboratory.[/say]")
 
 		saynn("[say=eliza]At least he cares more about me rather than some lab..[/say]")
 
-		saynn("[say=elizaMom]You're saying I don't care about you? You might be a bigger fool then. You almost died last time, you realize that? And without you.. what reason would I have?[/say]")
+		saynn("The older feline exhales sharply, her clawed digits pinching the bridge of her nose.")
 
-		saynn("Eliza pulls her gaze away.. and just stands, with her arms crossed. She doesn't have much against such a strong argument.")
+		saynn("[say=elizaMom]You're saying I don't care about you? Maybe I found a bigger fool then.[/say]")
 
-		saynn("[say=elizaMom]You think that science is all fun and games? It's not. It's dangerous.. but also boring.. which makes it extra dangerous for young minds like yours.[/say]")
+		saynn("That sounded harsh, Eliza's lips trembling slightly.")
 
-		saynn("[say=eliza]I'm not a young girl already, mom.. You can't kill the love for science in me..[/say]")
+		saynn("[say=elizaMom]You almost died, Eliza. If anything happened to you.. what reason would I have?[/say]")
 
-		saynn("[say=elizaMom]I know I can't. But I also can't let science kill you. So you will do as I say.[/say]")
+		saynn("Eliza pulls her gaze away.. and just stands, with her arms crossed. That argument strikes deep.")
 
-		saynn("The older woman furrows her brows when noticing the mag on the counter.")
+		saynn("[say=elizaMom]Science isn't a game. It's dangerous. But it's also boring, which makes it even worse for young minds like yours.[/say]")
 
-		saynn("Eliza sighs, her paws reach for that mag and bring it up to her lips, the words 'Best mom' that are written on it are clearly visible. Hm..")
+		saynn("[say=eliza]I'm not a young girl already, mom.. You can't kill my love for science..[/say]")
 
-		saynn("[say=eliza]Is that why you came here.. That's the first time I see you in a while..[/say]")
+		saynn("Eliza exhales through her nose, tail flicking irritably.")
+
+		saynn("[say=elizaMom]I know I can't. But I can stop it from killing you.[/say]")
+
+		saynn("The older feline's gaze softens.. if only slightly.")
+
+		saynn("[say=elizaMom]So you're going to do as I say.[/say]")
+
+		saynn("The older woman furrows her brows when noticing the mag on the counter. Eliza quickly snatches it up and starts sipping coffee out of it, her grip strong.")
+
+		saynn("[say=eliza]Is that why you came here, to lecture me in person.. That's the first time I see you in a while..[/say]")
 
 		saynn("[say=elizaMom]I had some business with the captain. Thought I might as well see how you are doing.[/say]")
 
-		saynn("She looks around and shakes her head subtly, seeing.. the state of things.")
+		saynn("Her gaze sweeps across the workplace again.")
 
 		saynn("[say=elizaMom]But now I'm thinking about taking you with me.[/say]")
 
 		saynn("Eliza's eyes brighten up, her feline ears perk.")
 
-		saynn("[say=eliza]Oh? You're taking me back into the big lab?[/say]")
+		saynn("[say=eliza]Oh? You mean.. you want me to join you in the big lab?[/say]")
 
-		saynn("There is a lot of hope in that question.. But you can already hear the dreams crashing and smashing..")
+		saynn("You can hear the unspoken excitement.. Dreams and hopes fill the whole room.. but not for long.")
 
-		saynn("[say=elizaMom]What? No. Sweetheart, we just talked about it. Your science days are over, I will find you a different workplace.. One.. that's not so.. dirty.[/say]")
+		saynn("[say=elizaMom]What? No. Sweetheart, we just talked about it. Your science days are over, I will find you a different workplace.. One.. that's not so..[/say]")
+
+		saynn("Her lip curls slightly as she gestures vaguely around the room.")
+
+		saynn("[say=elizaMom]Dirty.[/say]")
 
 		saynn("Dirty? What is she talking about? Compared to the cellblock, this lobby is sterile.. pristine.")
 
-		saynn("[say=eliza]But mom! I didn't blow up the lab even once here! We've been making a lot of progress! Look.[/say]")
+		saynn("[say=eliza]But mom! I didn't blow up a single lab here! We've been making a lot of progress! Look.[/say]")
 
-		saynn("She opens the laptop.. and shows her mother something on it.")
+		saynn("She hastily opens her laptop.. scrolling through files before showing something to her mom.")
 
 		saynn("[say=eliza]See? We've been slowly filling up this database. This prison is turning out to be an invaluable source of unique.. science discoveries. I can tell you about each one, they all seem to possess some kind of transformative properties.. isn't that cool?[/say]")
 
-		saynn("[say=elizaMom]This is dangerous, Eliza.. How can you not see that?[/say]")
+		saynn("The older female scans the screen.. her expression unreadable.")
 
-		saynn("[say=eliza]But I love doing it. I wanna keep doing it. You should see what shitty equipment we have here.. but we're making it work! We're doing science! Any other job would.. I don't know.. any other job would make me feel miserable.[/say]")
+		saynn("[say=elizaMom]Yeah.. You remind me about how dangerous we really are..[/say]")
 
-		saynn("Her raw passion is making you feel something fuzzy inside. And you're probably not alone in this.")
+		saynn("[say=eliza]But I love doing it. I wanna keep doing it. You should see what shitty equipment we have here.. but we're making it work! We're doing science! Any other job would.. I don't know..[/say]")
 
-		saynn("[say=elizaMom]Alright.. alright. Don't make me seem like a monster. Who am I to rob you of all this?[/say]")
+		saynn("she exhales softly.")
+
+		saynn("[say=Eliza]Any other job would make me feel miserable.[/say]")
+
+		saynn("For a long moment, there is silence. Her raw passion is making you feel something fuzzy inside. And you're probably not alone in this. Eliza's mother studies her daughter's eyes.")
+
+		saynn("[say=elizaMom]Alright.. alright. Don't make out to be some kind of villain. Who am I to rob you of this life?[/say]")
 
 		saynn("[say=eliza]You're my mom.. mom.. I'm gonna be the best chemist, I'm gonna make you proud![/say]")
 
+		saynn("Looks like a good talk is all they needed.")
+
 		saynn("Eliza's mother puts on a polite smile.")
 
-		saynn("[say=elizaMom]Hm. I have a question for you, sweetheart.[/say]")
+		saynn("[say=elizaMom]We will see about that~.[/say]")
+
+		saynn("She pauses.")
+
+		saynn("[say=elizaMom]Hm. I do have a question for you, sweetheart.[/say]")
 
 		saynn("[say=eliza]Yes?[/say]")
 
-		saynn("[say=elizaMom]You said.. we.. a few times. You're not working alone?[/say]")
+		saynn("[say=elizaMom]You kept saying.. we.. You're not working alone?[/say]")
 
-		saynn("[say=eliza]Oh yeah![/say]")
+		saynn("Eliza blinks.. and then grins, her ears perking up as she hastily scans the room.")
 
-		saynn("Eliza hastily looks around.. and notices you.")
+		saynn("Her eyes find you.")
 
-		saynn("[say=eliza]There you are! Come here![/say]")
+		saynn("[say=eliza]Oh! There you are! Come here![/say]")
 
-		saynn("Time to introduce yourself..")
+		saynn("You inhale. Time to introduce yourself..")
 
 		addButton("Introduction", "See what Eliza's mother thinks about you", "intro_you")
 	if(state == "intro_you"):
 		playAnimation(StageScene.Duo, "stand", {npc="elizaMom"})
-		saynn("As you approach the pair, you are met by a sharp, judging gaze.")
+		saynn("As you step closer, you can feel it.. the weight of a sharp, scrutinizing gaze pinning you in place. The older feline's eyes are dissecting every inch with a silent, clinical judgment. It's the kind of look that makes you instinctively stand up straighter.. as if that can somehow erase the fact that you're wearing a collar.")
 
 		saynn("[say=eliza]This is {pc.name}, my assistant.[/say]")
 
-		saynn("You straighten your back, trying to look.. presentable. But you can just hear the silent 'Ew' coming from Eliza's mother.")
+		saynn("There is a brief, heavy silence.. but you can practically hear the unspoken 'Ew' lingering in the air.")
 
 		saynn("[say=eliza]And this is my mom, Scarlet Quinn![/say]")
 
+		saynn("You clear your throat.. trying to sound as polite.. and non-criminal.. as possible.")
+
 		saynn("[say=pc]Uh. Pleased to meet you, Miss Quinn.[/say]")
 
-		saynn("[say=elizaMom]Missus Quinn.[/say]")
+		saynn("[say=elizaMom]Mrs. Quinn.[/say]")
 
-		saynn("Right.. is that even a word.")
+		saynn("Right.. You blink. Not the best start.")
 
 		saynn("[say=pc]My bad.[/say]")
 
@@ -121,63 +153,109 @@ func _run():
 
 		saynn("[say=elizaMom]Can't help but notice.. you're working with an inmate.[/say]")
 
+		saynn("There is no subtlety in her tone.. just pure, unfiltered distaste. Eliza, unfazed, simple grins and shrugs.")
+
 		saynn("[say=eliza]Yeah? {pc.He} {pc.has} been a huge help![/say]")
 
-		saynn("[say=elizaMom]They have a collar around their neck.[/say]")
+		saynn("Scarlet's gaze flicks toward your collar.")
 
-		saynn("[say=eliza]Yeah, inmates have those here, very handy~.[/say]")
+		saynn("[say=elizaMom]They have a shock collar around their neck.[/say]")
+
+		saynn("Her voice is laced with something between disgust and disbelief.")
+
+		saynn("[say=eliza]Yeah, inmates have those here. Super handy~.[/say]")
+
+		saynn("Scarlet exhales through her nose.")
 
 		saynn("[say=elizaMom]I begin.. to regret my decision, yet again.[/say]")
 
+		saynn("Eliza rolls her eyes.")
+
 		saynn("[say=eliza]Mo-o-om.[/say]")
 
-		saynn("[say=elizaMom]How can you.. lower yourself.. to their level. I gotta get you out of here, back into civilization, away from all this..[/say]")
+		saynn("[say=elizaMom]How can you.. lower yourself.. to their level. I gotta get you out of here, back into civilization, away from.. this..[/say]")
 
 		saynn("[say=eliza]Nope! Too late! You already agreed to let me stay.[/say]")
 
+		saynn("She puts on a triumphant grin.")
+
 		saynn("[say=elizaMom]Khm..[/say]")
 
-		saynn("[say=eliza]But if you wanna offer me to work in the big lab again.. I might even agree~.[/say]")
+		saynn("[say=eliza]But if you wanna offer me a position in our big lab.. I might even say yes~.[/say]")
+
+		saynn("There is a hidden note of hope in her voice. Scarlet only gives her a look.")
 
 		saynn("[say=elizaMom]Fine, stay. I'm a woman of my word. I advise you to be careful. Especially.. yeah.[/say]")
 
+		saynn("Her eyes flick toward you. She makes it painfully obvious. Eliza huffs.")
+
 		saynn("[say=eliza]I won't blow up this lab, I promise![/say]")
 
-		saynn("[say=elizaMom]That's not the only thing I'm worrying about. Don't lose yourself, Eliza. Remember who you are. Now, if you excuse me, I gotta exchange a few more words with the captain.[/say]")
+		saynn("Scarlet's jaw tightens.")
 
-		saynn("[say=eliza]Be well, mom![/say]")
+		saynn("[say=elizaMom]That's not the only thing I'm worrying about. Remember who you are.[/say]")
 
-		saynn("[say=elizaMom]You too.[/say]")
+		saynn("The air is thick with unspoken weight.")
 
-		saynn("And just like that, Scarlet Quinn steps out from behind the counter.. and enters the elevator. Following her.. are a few guards.")
+		saynn("[say=elizaMom]Now, if you excuse me, I gotta exchange a few words with the captain.[/say]")
+
+		saynn("[say=eliza]Be well, mom~![/say]")
+
+		saynn("[say=elizaMom]No need to sing goodbyes yet.[/say]")
+
+		saynn("And just like that, Scarlet Quinn steps out from behind the counter.. and enters the elevator. Following her.. are her guards.")
 
 		addButton("Continue", "See what happens next", "after_mom")
 	if(state == "after_mom"):
 		removeCharacter("elizaMom")
 		playAnimation(StageScene.Duo, "stand", {npc="eliza"})
+		saynn("You watch the elevator doors close behind Scarlet Quinn, the last flick of her tail vanishing from sight. But the air still feels heavy with her presence.")
+
 		saynn("[say=pc]Your mother doesn't like me much.[/say]")
 
-		saynn("[say=eliza]Well, you're nothing but dirty criminal scum in her eyes. I had the same feeling when I first arrived here.[/say]")
+		saynn("Eliza lets out a small snicker, learning against the counter with an amused flick of her ear.")
+
+		saynn("[say=eliza]Well, to be fair, you're nothing but dirty criminal scum in her eyes.[/say]")
+
+		saynn("So rude.")
+
+		saynn("[say=pc]Wow. Thanks.[/say]")
+
+		saynn("[say=eliza]Hey, I'm just saying it like it is! I felt exactly the same way when I first got here.[/say]")
 
 		saynn("[say=pc]But that changed?[/say]")
 
-		saynn("[say=eliza]Something changed. And also, my mom doesn't realize that inmates make for great test subjects~.[/say]")
+		saynn("Eliza pauses, tilting her head and thinking for some time.")
 
-		saynn("Right..")
+		saynn("[say=eliza]Maybe.[/say]")
 
-		saynn("[say=eliza]Maybe you helping me discover more drugs will make her change her mind about you.[/say]")
+		saynn("There is a flicker of something unreadable in her eyes.. but it fades quickly.")
+
+		saynn("[say=eliza]Besides, my mom doesn't realize that inmates make for great test subjects~.[/say]")
+
+		saynn("Right.. Of course.")
+
+		saynn("[say=eliza]She might warm up to you if she sees how helpful you are~.[/say]")
 
 		saynn("[say=pc]Somehow I doubt it. We're on different levels.. or whatever she said.[/say]")
 
-		saynn("Eliza shrugs.")
+		saynn("Eliza lets out a chuckle, her tail swaying behind her.")
 
-		saynn("[say=eliza]There are ways to get down to the same levels~. Like kneeling.[/say]")
+		saynn("[say=eliza]Well.. there are ways to get down to the same level~..[/say]")
 
-		saynn("She chuckles.")
+		saynn("She shoots you a playful grin.")
 
-		saynn("[say=eliza]I know that finding new drugs is gonna be harder now, since you found so many already.. I have a lead on something interesting that might help with this. I will tell you when I get more information~. Keep it up.[/say]")
+		saynn("[say=eliza]Climbing up![/say]")
 
-		saynn("You nod.")
+		saynn("A small pause..")
+
+		saynn("[say=eliza]Or kneeling down~.[/say]")
+
+		saynn("You roll your eyes as she laughs at her own joke.")
+
+		saynn("[say=eliza]Anyway~. I know that finding new drugs is gonna be harder now, since you found so many already.. I have a lead on something interesting that might help with this. I will tell you when I get more information~. Keep it up.[/say]")
+
+		saynn("You nod. She taps a claw lightly against your chest.. before pulling away.")
 
 		saynn("Time to go.")
 
