adf00e3b0c64533c6d0526e85bff0c581f51ff26
Rahi <vip-ak47@yandex.ru>
2025-02-15 17:05:28 +0300

    Moving the portable mode code to be called as early as possible

--- Game/GM.gd
+++ Game/GM.gd
@@ -14,21 +14,6 @@ func _init():
 
 func _ready():
 	var directory = Directory.new()
-
-	var exe_dir = OS.get_executable_path().get_base_dir()
-	print("Current Dir:", exe_dir)
-	var portable_dir = exe_dir.plus_file("BDCC.data")
-
-	if Directory.new().dir_exists(portable_dir):
-		print("Using portable save directory:", portable_dir)
-
-		if not Directory.new().dir_exists(portable_dir + "/Godot/app_userdata/BDCC"):
-			Directory.new().make_dir_recursive(portable_dir + "/Godot/app_userdata/BDCC")
-		
-		if OS.has_environment("APPDATA"):
-			OS.set_environment("APPDATA", portable_dir)  # Windows
-		if OS.has_environment("HOME"):
-			OS.set_environment("HOME", portable_dir)  # Linux & Mac
 		
 	directory.make_dir("user://saves")
 	directory.make_dir("user://mods")
