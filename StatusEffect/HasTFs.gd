9bb3564a51f1dcc0ab5e6b15e57035784a00cd46
Rahi <vip-ak47@yandex.ru>
2025-04-26 19:29:03 +0300

    A much better tf status effect

--- StatusEffect/HasTFs.gd
+++ StatusEffect/HasTFs.gd
@@ -21,15 +21,33 @@ func getEffectDesc():
 	if(tfHolder == null):
 		return "You shouldn't see this.."
 	var tfCount:int = tfHolder.getActiveTransformationsCount()
-	var theText:String = "You are under the effects of "+str(tfCount)+" unknown transformation"+("s" if tfCount != 1 else "")+".."
 	
-	var canScanAmount:int = GM.main.SCI.getTFsCanScanAmount(character)
-	if(canScanAmount > 0):
-		theText += "\n\n"+str(canScanAmount)+" transformation"+("s" if canScanAmount != 1 else "")+" are ready to be scanned!"
-
+	#var theText:String = "You are under the effects of "+str(tfCount)+" unknown transformation"+("s" if tfCount != 1 else "")+".."
+	var theText:String = "You are under the effects of "+str(tfCount)+" drug"+("s" if tfCount != 1 else "")+":"
+	
+	for tfa in tfHolder.transformations:
+		var tf:TFBase = tfa
+		var tfID:String = tf.id
+		var pillName:String = tf.getPillName()
+		var isUnlocked:bool = GM.main.SCI.isTransformationUnlocked(tfID)
+		var isTested:bool = GM.main.SCI.isTransformationTested(tfID)
+		var canBeTestedIfUnlocked:bool = tf.canBeTested()
+		var reachedLastStage:bool = !tf.canTransformFurther()
+		
+		theText += "\n - "+(pillName if isUnlocked else "Unknown drug")
+		if(!isTested && isUnlocked && canBeTestedIfUnlocked):
+			theText += " (Can be scanned by Eliza!)"
+		elif(reachedLastStage):
+			theText += " (Last stage reached)"
+		elif(tf.getStage() >= 1):
+			theText += " (stage "+str(tf.getStage())+")"
+	
 	return theText
 	
 func getEffectImage():
+	var canScanAmount:int = GM.main.SCI.getTFsCanScanAmount(character)
+	if(canScanAmount > 0):
+		return "res://Images/StatusEffects/dna1-can-scan.png"
 	return "res://Images/StatusEffects/dna1.png"
 
 func getIconColor():
