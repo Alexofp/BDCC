3317af39cbd9b8e49f1e20f370f70eb768eaafbf
keerifox <keerifox@users.noreply.github.com>
2025-05-02 22:34:56 +0000

    prevent most PunishInteraction actions with restraints on arms or paws

--- Modules/NpcSlaveryModule/Enslaving/EnslaveDynamicNpcEvent.gd
+++ Modules/NpcSlaveryModule/Enslaving/EnslaveDynamicNpcEvent.gd
@@ -30,11 +30,15 @@ func run(_triggerID, _args):
 			return
 		
 		if(!character.hasEnslaveQuest() ):
-			if(character.getInventory().hasEquippedItemWithTag(ItemTag.AllowsEnslaving)):
+			var pcRestraintsPreventChoking:bool = GM.pc.hasBlockedHands() || GM.pc.hasBoundArms()
+			if(!pcRestraintsPreventChoking && character.getInventory().hasEquippedItemWithTag(ItemTag.AllowsEnslaving)):
 				addButton("Enslave!", "Try to enslave them", "doenslave", [_args[0]])
 			else:
 				if(_triggerID == Trigger.DefeatedDynamicNPC):
-					addDisabledButton("Enslave!", "They need to be wearing a collar for you to be able to enslave them")
+					if(pcRestraintsPreventChoking):
+						addDisabledButton("Enslave!", "You can't do that with restraints on your arms..")
+					else:
+						addDisabledButton("Enslave!", "They need to be wearing a collar for you to be able to enslave them")
 		
 
 func getPriority():
