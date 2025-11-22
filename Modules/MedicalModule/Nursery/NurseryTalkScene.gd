c4ea177439709671c30cc971c262cbb11596adb4
Selinoccino <laniusowo69@gmail.com>
2025-11-22 10:59:23 +0200

    minor spelling and punctuation fixes, + other small changes
    - changed all single quotation things in the affected files to a different one i saw used in the intro scene (visually identical, different character code)
    - fixed some slight capitalization stuff in nurseryTalkScene
    - all ellipses in the affected files now have three dots (unless i missed one)
    - the nurse now has some extra flavor text when asking about the alphacorp drug if you have the "no hybrids" perk
    - minor spelling mistakes fixed
    - simplified childStr thing in nurseryTalkScene
    - changed the phrase "digital century" in TaviQuest2Skar to "digital age", since it's mentioned in the intro that they've been doing the mindtest for hundreds of years

--- Modules/MedicalModule/Nursery/NurseryTalkScene.gd
+++ Modules/MedicalModule/Nursery/NurseryTalkScene.gd
@@ -31,35 +31,35 @@ func _run():
 			
 #			saynn("You approach the nurse and try to get her attention. She drags her gaze away from the screen and looks at you.")
 			
-			saynn("You wander over to the nurse's desk, the little name placard says her name is 'Minuette'.")
+			saynn("You wander over to the nurse’s desk, the little name placard says her name is ’Minuette’.")
 
 			saynn("[say=pc]What’s this room for?[/say]")
 
 			saynn("The nurse barely looks up from her work on her reception computer.")
 			saynn("[say=nurse]This is a nursery, inmate. A place for the prison’s mothers.[/say]")
-			saynn("It's only then the nurse decides to look you over. Making brief eye contact.")
+			saynn("It’s only then the nurse decides to look you over. Making brief eye contact.")
 
 			if(GM.pc.isHeavilyPregnant()):
 				saynn("She then eyes you out and notices your pregnant belly.")
-				saynn("[say=nurse]Yeah, you'll need to come here soon enough.[/say]")
+				saynn("[say=nurse]Yeah, you’ll need to come here soon enough.[/say]")
 				if(GM.pc.getInmateType() == InmateType.HighSec):
-					saynn("[say=nurse]And, please do me a favor and don't smash the place like the other Red Block inmates. Please and Thank you.[/say]")
+					saynn("[say=nurse]And, please do me a favor and don’t smash the place like the other Red Block inmates. Please and thank you.[/say]")
 			elif(GM.pc.isVisiblyPregnant()):
 				saynn("She then eyes you out and notices your pregnant belly.")
 				
-				saynn("[say=nurse]You're too late for birth control and you're too early to deliver. You'll need to see me sometime later.[/say]")
+				saynn("[say=nurse]You’re too late for birth control and you’re too early to deliver. You’ll need to see me sometime later.[/say]")
 				if(GM.pc.getInmateType() == InmateType.HighSec):
-					saynn("[say=nurse]And, please do me a favor and don't smash the place like the other Red Block inmates. Please and Thank you.[/say]")
+					saynn("[say=nurse]And, please do me a favor and don’t smash the place like the other Red Block inmates. Please and thank you.[/say]")
 
 			if(GM.pc.isReadyToGiveBirth()):
 				saynn("She then eyes you out and notices your pregnant belly.")
 				saynn("[say=nurse]You look ready to drop already, if you wanna get that over with just let me know.[/say]")
 				if(GM.pc.getInmateType() == InmateType.HighSec):
-					saynn("[say=nurse]And, please do me a favor and don't smash the place like the other Red Block inmates. Please and Thank you.[/say]")
+					saynn("[say=nurse]And, please do me a favor and don’t smash the place like the other Red Block inmates. Please and thank you.[/say]")
 			saynn("Well then. You look around. It looks more like a lobby, a few benches are placed near the white walls. You notice a few pictures there and there, all of them clearly drawn by someone young. There are two other doors, apart from the one that you walked in through.")
 
 		else:
-			saynn("You approach the nurse's desk again, the same name placard of 'Minuette' stands atop it. She drags her gaze away from the screen and looks at you.")
+			saynn("You approach the nurse’s desk again, the same name placard of ’Minuette’ stands atop it. She drags her gaze away from the screen and looks at you.")
 
 			# (if player has the perk FertilityProudMom and didn't get an apple today)
 			if(receivedApple):
@@ -82,11 +82,11 @@ func _run():
 		
 		
 		addButton("How works", "Ask her how everything works", "how_works")
-		addButton("Working here", "Ask her what it's like working here.", "nursework")
+		addButton("Working here", "Ask her what it’s like working here.", "nursework")
 		
 		if(getModuleFlag("MedicalModule", "Nursery_AskedHowWorks", false)):
 			if(!getModuleFlag("MedicalModule", "Nursery_AskedDatabase", false)):
-				addButton("Database", "Is there really no way to interact with them", "children")
+				addButton("Database", "Is there really no way to interact with them?", "children")
 			else:
 				addButton("Database", "Take a look at your children", "database")
 		
@@ -131,35 +131,38 @@ func _run():
 
 		saynn("The nurse looks at your inmate number and starts punching buttons on her old clicky keyboard.")
 
-		saynn("[say=nurse]Let me see.. inmate number.. {pc.inmateNumber}. Yes, you went through one during your intake process. It was done by.. Doctor Quinn.[/say]")
+		saynn("[say=nurse]Let me see... inmate number... {pc.inmateNumber}. Yes, you went through one during your intake process. It was done by... Doctor Quinn.[/say]")
 
-		saynn("You remember being injected with something. But that was for something else. Did dr. Quinn lie to you?")
+		saynn("You remember being injected with something. But that was for something else. Did Dr. Quinn lie to you?")
 
 		saynn("[say=pc]What [i]exactly[/i] did I get?[/say]")
 
-		saynn("[say=nurse]Well... I'm not [i]supposed[/i] to tell an inmate like you... but you know, with the amount of crazy and belligerent inmates I have to deal with when they suddenly show and start yelling about a 'certain' fight they lost or what's happening to them. It makes my job rather difficult.[/say]")
+		saynn("[say=nurse]Well... I’m not [i]supposed[/i] to tell an inmate like you... but you know, with the amount of crazy and belligerent inmates I have to deal with when they suddenly show and start yelling about a ’certain’ fight they lost or what’s happening to them. It makes my job rather difficult.[/say]")
 		saynn("[say=nurse]And I just so happen to let the information slip in the hopes it makes my job slightly easier...[/say]")
 		
 		saynn("She motions to lean in closer so she can quickly whisper the answer to you.")
 		
-		saynn("[say=nurse]You got one the experimental variants of AlphaCorp's Fertility drugs, it should've completed by the time you woke up on your first day.[/say]")
-		saynn("[say=nurse]Don't worry about something [i]entirely[/i] weird happening to you, it still has benefits if you don't have a uterus, and all the staff get the same treatment for working in deep space, like this prison.[/say]")
+		saynn("[say=nurse]You got one of the experimental variants of AlphaCorp’s Fertility drugs, it should’ve completed by the time you woke up on your first day.[/say]")
+		saynn("[say=nurse]Don’t worry about something [i]entirely[/i] weird happening to you, it still has benefits if you don’t have a uterus, and all the staff get the same treatment for working in deep space, like this prison.[/say]")
 		saynn("[say=nurse]Namely, it should have massively boosted physical protection of your womb and any offspring you make, to the point anything in your womb will survive anything this place will throw at you.[/say]")
-		saynn("[say=nurse]And secondly, it will have altered how your offspring develops, progressing at a rate set by a Corporate Medical Officer. And as much as Corporate is full of idiots, they're smart enough to not let inmates give birth at random. Once someone starts feeling false contractions, they'll have to come here to get induced.[/say]")
+		saynn("[say=nurse]And secondly, it will have altered how your offspring develops, progressing at a rate set by a Corporate Medical Officer. And as much as Corporate is full of idiots, they’re smart enough to not let inmates give birth at random. Once someone starts feeling false contractions, they’ll have to come here to get induced.[/say]")
+		
+		if(GM.pc.hasPerk(Perk.StartNoHybrids)):
+			saynn("[say=nurse]You’re also one of the lucky ones whose reproductive system had a reaction to the drug, so now you can never have hybrid offspring.[/say]")
 
 		saynn("The nurse pushes you away and returns to her normal voice.")
 		saynn("[say=nurse]That should be everything, inmate.[/say]")
 
 		saynn("Curious... You rub your chin and look down at your belly.")
 
-		addButton("Continue", "That's nice", "")
+		addButton("Continue", "That’s nice", "")
 
 	if(state == "nursework"):
-		saynn("[say=pc]So, what is like working here?[/say]")
+		saynn("[say=pc]So, what’s it like working here?[/say]")
 		saynn("The nurse looks up at you with tired eyes, clearly mulling over how she should answer your question.")
 		saynn("[say=nurse]I work Labor and Delivery in a prison with already violent and degenerate inmates. A lot of inmates do not handle pregnancy well. Do you need more to get the idea?[/say]")
 		saynn("[say=pc]Uh, No thanks, I get it...[/say]")
-		saynn("[say=nurse]It's certainly not everyone that comes through here, but I can tell you I've seen the worst fights in this place.[/say]")
+		saynn("[say=nurse]It’s certainly not everyone that comes through here, but I can tell you I’ve seen the worst fights in this place.[/say]")
 		saynn("The nurse returns to her computer work while you mull over her words...")
 
 		addButton("Continue", "", "")
@@ -168,9 +171,10 @@ func _run():
 		
 		saynn("[say=pc]Is there really no way to check my children?[/say]")
 
-		saynn("[say=nurse]Not directly, no. Each time you give a birth or someone else does to your baby, we do add them to the database under your name. I can show you your records.[/say]")
+		saynn("[say=nurse]Not directly, no. Each time you or someone else gives birth to your baby, we do add them to the database under your name. I can show you your records.[/say]")
 
 		addButton("Database", "Take a look at your children", "database")
+		addButton("No thanks","You’d rather not","")
 
 	if(state == "database"):
 		saynn("You ask the nurse to show you the database records. She quickly punches some buttons and turns the screen towards you.")
@@ -245,7 +249,7 @@ func _run():
 			sayn(resultTable)
 			
 		
-		addButton("Continue", "That's nice", "")
+		addButton("Continue", "That’s nice", "")
 		
 		
 
@@ -270,7 +274,7 @@ func _run():
 
 		saynn("[say=nurse]Now get some sleep. Don’t worry, the therapy you went through prevents you from giving birth without it being specifically induced.[/say]")
 
-		saynn("The nurse helps you get settled, putting one of those heart monitors on your finger while another gets wrapped around your bare stomach. She continues to work but since she doesnt do anything directly to you again you lose all interest.")
+		saynn("The nurse helps you get settled, putting one of those heart monitors on your finger while another gets wrapped around your bare stomach. She continues to work but since she doesn’t do anything directly to you again you lose all interest.")
 
 		saynn("[say=nurse]All set, get some good rest...[/say]")
 
@@ -282,15 +286,15 @@ func _run():
 		playAnimation(StageScene.GivingBirth, "idle", {bodyState={naked=true}})
 		saynn("You sleep in the patient’s bed. It’s not the comfiest but it’s better than nothing.")
 
-		saynn("Welcome to day "+str(GM.main.getDays())+" of your sentence")
+		saynn("Welcome to day "+str(GM.main.getDays())+" of your sentence.")
 
-		saynn("As you open your eyes.. you see your own huge belly. Your eyes widen as you give it a explorative touch, and you blink in surprise when you feel it, amazed at just how fast this must have happened. There is no denying it, you are ready to give birth. You can barely sit down.")
+		saynn("As you open your eyes... you see your own huge belly. Your eyes widen as you give it a explorative touch, and you blink in surprise when you feel it, amazed at just how fast this must have happened. There is no denying it, you are ready to give birth. You can barely sit down.")
 
 		saynn("Unsure of what to really do aside from listen to the beeping heart montiors, you simply cradle your large middle with both hands, and simply wait for whatever nurse to show up.")
 
 		saynn("A dull ache in your belly gets worse as you wait, simply enduring it until the same nurse enters your room once again.")
 		
-		saynn("[say=nurse]Hello again, Time to go, come on, I'll help you.[/say]")
+		saynn("[say=nurse]Hello again. Time to go, come on, I’ll help you.[/say]")
 
 		addButton("Continue", "Time to create life", "startbirth")
 
@@ -310,26 +314,24 @@ func _run():
 		
 		saynn("You enter the birthing room with the help of the nurse. It’s spacious and has a special bed in the middle, the one that is designed to keep your legs spread. A few nurses come in and out to prepare as you get your collar clipped to the bed. You lay down and await your fate.")
 
-		saynn("The main nurse gets the birthing process started as soon as it's confirmed you're on the appropriate painkillers, letting things get going as the last preperations are made and the majority of the other nurses leave again.")
+		saynn("The main nurse gets the birthing process started as soon as it’s confirmed you’re on the appropriate painkillers, letting things get going as the last preperations are made and the majority of the other nurses leave again.")
 		
-		saynn("Your content to just let them do their work while you lay there and take deep breaths while your belly has its first cramps. Someone asks if they need to use the heavy restraints on you but someone else dismisses them.")
+		saynn("You’re content to just let them do their work while you lay there and take deep breaths while your belly has its first cramps." + (" Someone asks if they need to use the heavy restraints on you but someone else dismisses them." if(GM.pc.getInmateType() == InmateType.HighSec) else "") )
 		
-		saynn("Once your waters break everyone gets settled in. You follow the instructions the main delivery nurse gives you as nature takes it's course. And 'Nature' means incredible pain and discomfort.")
+		saynn("Once your waters break everyone gets settled in. You follow the instructions the main delivery nurse gives you as nature takes its course. And ’Nature’ means incredible pain and discomfort.")
 		
-		saynn("It feels like many hours as you work to push out your offspring, gradually stretching yourself out... Life certainly isnt easy and you feel like you're about to pass out...")
+		saynn("It feels like many hours as you work to push out your offspring, gradually stretching yourself out... Life certainly isn’t easy and you feel like you’re about to pass out...")
 		
 		# (if more than one child)
 		if(bornChildAmount > 1):
-			saynn("But things aren’t always that easy. Even after giving birth to the first child your belly only deflates slightly, still feeling the pain of contractions and the protesting movement of your remaining progeny, you’re clearly not done yet. Huffing and groaning as the pain continues..")
+			saynn("But things aren’t always that easy. Even after giving birth to the first child your belly only deflates slightly, still feeling the pain of contractions and the protesting movement of your remaining progeny, you’re clearly not done yet. Huffing and groaning as the pain continues...")
 
 		addButton("Continue", "See what happens", "continue1")
 
 	if(state == "continue1"):
 		playAnimation(StageScene.GivingBirth, "idle", {bodyState={naked=true}})
 		
-		var childStr = "child"
-		if(bornChildAmount > 1):
-			childStr = "children"
+		var childStr = "child" + ("ren" if(bornChildAmount>1) else "")
 		
 		saynn("You gave birth to "+str(bornChildAmount)+" "+childStr+"! The nurse congratulates you.")
 
