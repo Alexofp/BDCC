e642548392c1ea9dfef0f8c7ed92b6f99256a2e3
Void (Gabriel F) <gabriel@furuhjelm.com>
2025-12-03 19:02:34 -0800

    Fixed OS.shell_open() to work on OSX
    OS.shell_open() doesn't open the file directory on MacOS, so I added a small fix function to Util and then replaced all instances of "OS.shell_open" to use "Util.fixed_shell_open" instead.

--- UI/MainMenu/CreditsScreen.gd
+++ UI/MainMenu/CreditsScreen.gd
@@ -21,8 +21,8 @@ func _on_CloseButton_pressed():
 	emit_signal("onClosePressed")
 
 func _on_SupportRahiButton_pressed():
-	var _ok = OS.shell_open("https://subscribestar.adult/rahi")
+	var _ok = Util.fixed_shell_open("https://subscribestar.adult/rahi")
 
 
 func _on_RichTextLabel_meta_clicked(meta):
-	var _ok = OS.shell_open(meta)
+	var _ok = Util.fixed_shell_open(meta)
