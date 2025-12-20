b9563e42106619524b7e81774f0a69e173f55195
Frivolous <248780885+FrivolousBorks@users.noreply.github.com>
2025-12-19 20:51:56 -0800

    Add PeeProduction fluid tracker

---
+++ Player/FluidProduction/PeeProduction.gd
@@ -0,0 +1,36 @@
+extends FluidProduction
+class_name PeeProduction
+
+var pisser = null
+
+func _init(character):
+    pisser = character
+
+func getCapacity() -> float:
+    var mult: float = 1.0
+    var pc = getCharacter()
+    if pc != null:
+        mult += pc.getCustomAttribute(BuffAttribute.PissCapacityMult)
+
+    return 500 * mult
+
+func getProductionSpeedPerHour() -> float:
+    if !shouldProduce():
+        return 0.0
+
+    var mult = 1.0
+    var pc = getCharacter()
+    if pc != null:
+        mult += pc.getCustomAttribute(BuffAttribute.PissProductionMult)
+    
+    # The variance is arbitrary. It just felt weird always producing
+    # 100ml per hour.
+    var produced = (100 + RNG.randf_range(-30, 30)) * mult
+    print("produced " + str(produced) + " piss")
+    return produced
+
+func getFluidSource():
+    return FluidSource.Pissing
+
+func getCharacter():
+    return pisser
