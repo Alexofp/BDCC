d64431b7e8f321d233e2ca3f89d1d2e9747351d0
kristijanWolff <kristijano.bucalo@gmail.com>
2026-05-26 12:53:42 +0200

    FIx2 for the abbility to add custom mods for android
    this adds scrips neccecery to be able to use a new button in the mod browser on android as well as a pathway for the custom mod adding to work, as well as the re-naming logic inside so whean you upload a mod.zip file threw the internal http once you download the mod from the BDCC's mod launcher it auto checks the json file and re-names the zip acordingly,(as a quality of life improvement and the abbility to add multiple custom mods)

--- UI/ModBrowser/ModBrowser.gd
+++ UI/ModBrowser/ModBrowser.gd
@@ -23,9 +23,31 @@ func _ready():
 func _on_ModBrowser_visibility_changed():
 	if(visible && !downloadedMods):
 		downloadedMods = true
+		
+		# 1. Clear out placeholder layout elements instantly
+		resetMods()
+		
+		# 2. Inject your local test mod straight into the active array
+		var localModEntry = ModEntry.new()
+		localModEntry.name = "★ Local Custom Mod (Injected) ★"
+		localModEntry.author = "Localhost"
+		localModEntry.description = "Anonymously hosted zip archive loaded from your device via QRServ loopback background connection."
+		localModEntry.modversion = "1.0"
+		localModEntry.gameversion = "*"
+		localModEntry.download = "http://127.0.0.1:8080/mod.zip"
+		localModEntry.index = 0
+		allMods.append(localModEntry)
+		
+		# 3. Fetch Alex's full catalog from GitHub
 		var error = http_request.request("https://raw.githubusercontent.com/Alexofp/BDCCMods/main/allmods.json")
 		if error != OK:
 			Log.printerr("[ModBrowser] An error occurred in the HTTP request.")
+		
+		sortType = "newest"
+		
+		# 4. Give Godot a brief moment to open the UI containers before rendering cards
+		yield(get_tree(), "idle_frame")
+		updateModList()
 
 func resetMods():
 	pickedModEntry = null
@@ -89,7 +111,24 @@ func _on_HTTPRequest_request_completed(result, _response_code, _headers, body):
 		_i += 1
 		
 		allMods.append(newModEntry)
-
+		
+	var already_has_local = false
+	for checkingMod in allMods:
+		if checkingMod.name == "★ Local Custom Mod (Injected) ★":
+			already_has_local = true
+			break
+			
+	if not already_has_local:
+		var localModEntry = ModEntry.new()
+		localModEntry.name = "★ Local Custom Mod (Injected) ★"
+		localModEntry.author = "Localhost"
+		localModEntry.description = "Anonymously hosted zip archive loaded from your device via QRServ loopback background connection."
+		localModEntry.modversion = "1.0"
+		localModEntry.gameversion = "*"
+		localModEntry.download = "http://127.0.0.1:8080/mod.zip"
+		localModEntry.index = 0 
+		allMods.insert(0, localModEntry) 
+	
 	sortType = "newest"
 	allMods.sort_custom(self, "sort_newest")
 	updateModList()
@@ -125,9 +164,6 @@ func updatePickedModEntry():
 func onModEntrySelected(theModEntry):
 	pickedModEntry = theModEntry
 	updatePickedModEntry()
-	
-	
-
 
 func _on_DownloadModButton_pressed():
 	if(pickedModEntry == null):
@@ -144,7 +180,6 @@ func _on_DownloadModButton_pressed():
 	else:
 		Log.print("Downloading mod: "+str(pickedModEntry.download))
 
-
 func _on_HTTPRequestMod_request_completed(result, _response_code, _headers, _body):
 	downloadingContainer.visible = false
 
@@ -153,12 +188,68 @@ func _on_HTTPRequestMod_request_completed(result, _response_code, _headers, _bod
 		showMessage("Couldn't download mod")
 		return
 	
-	Log.print("Mod downloaded")
+	Log.print("Mod downloaded successfully.")
+
+	# --- ULTIMATE ANY-JSON AUTO-RENAMER ---
+	if pickedModEntry != null and pickedModEntry.download.get_file() == "mod.zip":
+		var modsFolder = GlobalRegistry.getModsFolder()
+		var currentPath = modsFolder.plus_file("mod.zip")
+		
+		var dir = Directory.new()
+		if dir.file_exists(currentPath):
+			var detected_name = ""
+			
+			# Mount the zip file into Godot's temporary memory
+			if ProjectSettings.load_resource_pack(currentPath):
+				var search_dir = Directory.new()
+				
+				# Open the temporary root folder where the zip files are loaded
+				if search_dir.open("res://") == OK:
+					search_dir.list_dir_begin(true, true) # Skip hidden system files
+					var file_name = search_dir.get_next()
+					
+					# Scan EVERY file inside the zip until we find one ending in .json
+					while file_name != "":
+						if not search_dir.current_is_dir() and file_name.get_extension().to_lower() == "json":
+							# WE FOUND IT! Whether it's looter.json, getting.json, or mod.json
+							var file = File.new()
+							if file.open("res://".plus_file(file_name), File.READ) == OK:
+								var json_text = file.get_as_text()
+								file.close()
+								
+								var json_parsed = JSON.parse(json_text)
+								if json_parsed.error == OK and json_parsed.result is Dictionary:
+									var mod_metadata = json_parsed.result
+									if mod_metadata.has("name"):
+										detected_name = str(mod_metadata["name"]).strip_edges()
+									elif mod_metadata.has("id"):
+										detected_name = str(mod_metadata["id"]).strip_edges()
+							break # Stop looking since we found our JSON file!
+						file_name = search_dir.get_next()
+					search_dir.list_dir_end()
+			
+			# Clean and rename the file based on what we read inside that random JSON file
+			if detected_name != "":
+				var illegal_chars = ["/", "\\", "?", "%", "*", ":", "|", '"', "<", ">"]
+				for ch in illegal_chars:
+					detected_name = detected_name.replace(ch, "")
+				
+				var newPath = modsFolder.plus_file(detected_name + ".zip")
+				
+				if dir.file_exists(newPath):
+					dir.remove(newPath)
+				
+				var rename_error = dir.rename(currentPath, newPath)
+				if rename_error == OK:
+					Log.print("[ModBrowser] Dynamic Renamer Success: " + detected_name + ".zip")
+				else:
+					Log.printerr("[ModBrowser] Rename failed. OS Error: " + str(rename_error))
+			else:
+				Log.printerr("[ModBrowser] Failed to find or parse any valid .json configuration file.")
 
 func showMessage(text):
 	messageDialog.dialog_text = text
 	messageDialog.show()
-
 
 static func sort_name(a, b):
 	if a.name < b.name:
@@ -180,12 +271,10 @@ func _on_SortNameButton_pressed():
 	allMods.sort_custom(self, "sort_name")
 	updateModList()
 
-
 func _on_SortNewestFirstButton_pressed():
 	sortType = "newest"
 	allMods.sort_custom(self, "sort_newest")
 	updateModList()
-
 
 func _on_SortOldestFirstButton_pressed():
 	sortType = "oldest"
@@ -195,10 +284,8 @@ func _on_SortOldestFirstButton_pressed():
 func _on_CloseButton_pressed():
 	emit_signal("closePressed")
 
-
 func _on_RichTextLabel_meta_clicked(meta):
 	var _ok = Util.fixed_shell_open(meta)
-
 
 func _on_ModSearch_text_changed(new_text: String):
 	if new_text.length() > 2:
@@ -213,4 +300,3 @@ func _on_ModSearch_text_changed(new_text: String):
 		updateModList(modsFound)
 	if new_text.length() == 0:
 		updateModList()
-	
