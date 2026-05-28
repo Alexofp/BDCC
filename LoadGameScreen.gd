01884c2a29568aaaccaf1dcbb6266543340e85f0
kristijanWolff <kristijano.bucalo@gmail.com>
2026-05-28 21:09:39 +0200

    Fixes the export/import save files for A16
    Fixes the export/import save files for android 16+ by changing it from documens folder to downloads.

---
+++ LoadGameScreen.gd
@@ -0,0 +1,229 @@
+extends Control
+
+onready var savesContainer = $VBoxContainer/ScrollContainer/ScrollVBox/SavesContainer
+var saveGameElemenetScene = preload("res://UI/MainMenu/SaveGameElement.tscn")
+signal onClosePressed
+var inDeleteMode = false
+var currentExportedPath = ""
+signal in_focus
+
+func _ready():
+	# Automatically setup our public folder structure on launch
+	ensure_external_save_folder_exists()
+	updateSaves()
+	
+	if(OS.get_name() == "Android"):
+		$VBoxContainer/GridContainer/SavesButton.visible = false
+	
+	if(OS.get_name() == "HTML5"):
+		$VBoxContainer/GridContainer/SavesButton.visible = false
+	
+	if OS.get_name() == "HTML5" and OS.has_feature("JavaScript"):
+		_define_js()
+		
+func ensure_external_save_folder_exists():
+	if OS.get_name() == "Android":
+		var download_path = OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS) + "/"
+		var target_folder = download_path + "BDCCSaves/"
+		
+		var dir = Directory.new()
+		if not dir.dir_exists(target_folder):
+			dir.make_dir_recursive(target_folder)
+		
+func updateSaves():
+	Util.delete_children(savesContainer)
+	
+	var savesPaths = SAVE.getSavesSortedByDate()
+	
+	for savePath in savesPaths:
+		var saveGameElementObject = saveGameElemenetScene.instance()
+		savesContainer.add_child(saveGameElementObject)
+		saveGameElementObject.setSaveFile(savePath)
+		saveGameElementObject.connect("onLoadButtonPressed", self, "onSaveLoadButtonClicked")
+		saveGameElementObject.connect("onDeleteButtonPressed", self, "onDeleteButtonClicked")
+		saveGameElementObject.connect("onExportButtonPressed", self, "onExportButtonClicked")
+		saveGameElementObject.setDeleteMode(inDeleteMode)
+		
+func onSaveLoadButtonClicked(savePath):
+	SAVE.switchToGameAndLoad(savePath)
+
+func onDeleteButtonClicked(savePath):
+	SAVE.deleteSave(savePath)
+	updateSaves()
+
+func _on_CloseButton_pressed():
+	emit_signal("onClosePressed")
+
+
+func _on_LoadGameScreen_visibility_changed():
+	if(visible):
+		updateSaves()
+
+
+func _on_DeleteButton_pressed():
+	inDeleteMode = !inDeleteMode
+	updateSaves()
+
+func onExportButtonClicked(savePath: String):
+	print("EXPORT: "+savePath)
+	if(OS.get_name() == "HTML5"):
+		var save_game = File.new()
+		if not save_game.file_exists(savePath):
+			return # Error! We don't have a save to load.
+		
+		save_game.open(savePath, File.READ)
+		#var saveData = parse_json(save_game.get_as_text())
+		
+		JavaScript.download_buffer(save_game.get_as_text().to_utf8(), savePath.get_file(), "text/plain")
+		save_game.close()
+		return
+	
+	if(OS.get_name() == "Android"):
+		var d = Directory.new()
+		var externalDir:String = OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS)
+		var finalDir = externalDir.plus_file("BDCCSaves/")
+		d.make_dir_recursive(finalDir)
+		var finalPath = finalDir.plus_file(savePath.get_file())
+		d.copy(savePath, finalPath)
+		$SaveExportedAlert.dialog_text = "Your save was exported to:\n"+str(finalPath)
+		$SaveExportedAlert.popup_centered()
+		return
+	
+	currentExportedPath = savePath
+	$ExportSaveDialog.current_file = savePath.get_file()
+	#$ExportSaveDialog.current_dir = OS.get_user_data_dir()
+	$ExportSaveDialog.popup_centered()
+	
+func _on_ExportSaveDialog_file_selected(path):
+	if(currentExportedPath == null || currentExportedPath == ""):
+		return
+	var d = Directory.new()
+	d.copy(currentExportedPath, path)
+
+func _notification(notification: int) -> void:
+	if notification == MainLoop.NOTIFICATION_WM_FOCUS_IN:
+		emit_signal("in_focus")
+
+# Copied from https://github.com/Orama-Interactive/Pixelorama/blob/master/src/Autoload/HTML5FileExchange.gd
+# Thanks to Pixelorama devs
+func _define_js():
+	# Define JS script
+	JavaScript.eval(
+		"""
+	var fileData;
+	var fileType;
+	var fileName;
+	var canceled;
+	function upload_save() {
+		canceled = true;
+		var input = document.createElement('INPUT');
+		input.setAttribute("type", "file");
+		input.setAttribute("accept", ".save");
+		input.click();
+		input.addEventListener('change', event => {
+			if (event.target.files.length > 0){
+				canceled = false;}
+			var file = event.target.files[0];
+			var reader = new FileReader();
+			fileType = file.type;
+			fileName = file.name;
+			reader.readAsText(file);
+			reader.onloadend = function (evt) {
+				if (evt.target.readyState == FileReader.DONE) {
+					fileData = evt.target.result;
+				}
+			}
+		});
+	}
+	""",
+		true
+	)
+	
+func readSaveFileHTML5():
+	if OS.get_name() != "HTML5" or !OS.has_feature("JavaScript"):
+		return
+
+	# Execute JS function
+	JavaScript.eval("upload_save();", true)  # Opens prompt for choosing file
+
+	yield(self, "in_focus")  # Wait until JS prompt is closed
+
+	yield(get_tree().create_timer(0.5), "timeout")  # Give some time for async JS data load
+
+	if JavaScript.eval("canceled;", true):  # If File Dialog closed w/o file
+		return
+
+	# Use data from png data
+	var file_data
+	while true:
+		file_data = JavaScript.eval("fileData;", true)
+		if file_data != null:
+			break
+		yield(get_tree().create_timer(1.0), "timeout")  # Need more time to load data
+
+#	var file_type = JavaScript.eval("fileType;", true)
+	var file_name = JavaScript.eval("fileName;", true)
+	
+	return [file_name, file_data]
+
+func _on_ImportButton_pressed():
+	if OS.get_name() == "HTML5":
+		var saveDataAndFileName = yield(readSaveFileHTML5(), "completed")
+		if(saveDataAndFileName == null || saveDataAndFileName.size() != 2):
+			return
+		SAVE.saveGameFromText(saveDataAndFileName[0], saveDataAndFileName[1])
+		updateSaves()
+	elif OS.get_name() == "Android":
+		var external_folder = OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS) + "/BDCCSaves/"
+		var internal_save_path = "user://saves/"
+		
+		var dir = Directory.new()
+		
+		# Ensure internal path is fully prepared
+		if not dir.dir_exists(internal_save_path):
+			dir.make_dir_recursive(internal_save_path)
+			
+		if dir.open(external_folder) == OK:
+			dir.list_dir_begin(true)
+			var file_name = dir.get_next()
+			
+			var success_count = 0
+			
+			while file_name != "":
+				if not dir.current_is_dir() and file_name.ends_with(".save"):
+					var source = external_folder + file_name
+					var destination = internal_save_path + file_name
+					
+					if dir.copy(source, destination) == OK:
+						success_count += 1
+						
+				file_name = dir.get_next()
+			dir.list_dir_end()
+			
+			# Force visual update so user instantly sees new options
+			updateSaves()
+			
+			# Give a nice system pop up confirming success
+			if success_count > 0:
+				$SaveExportedAlert.dialog_text = "Successfully imported " + str(success_count) + " save files!"
+				$SaveExportedAlert.window_title = "Import Complete"
+				$SaveExportedAlert.popup_centered()
+			else:
+				$SaveExportedAlert.dialog_text = "No custom files ending in .save were found in Download/BDCCSaves/"
+				$SaveExportedAlert.window_title = "Import Empty"
+				$SaveExportedAlert.popup_centered()
+		else:
+			print("Error: Could not scan target directory.")
+	else:
+		# Keep original file fallback behavior active for desktop machines
+		$ImportSaveDialog.popup_centered()
+
+func _on_ImportSaveDialog_file_selected(path: String):
+	print(path.get_file().get_basename())
+	var d = Directory.new()
+	d.copy(path, "user://saves/"+path.get_file().get_basename()+".save")
+	updateSaves()
+
+
+func _on_SavesButton_pressed():
+	var _ok = Util.fixed_shell_open(ProjectSettings.globalize_path("user://saves/"))
