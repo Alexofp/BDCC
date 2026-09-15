2edf4ef7a475b4da4a0bb9b5df6ea96bdba32ee5
Rahi <vip-ak47@yandex.ru>
2026-09-15 19:30:30 +0300

    Work work on the third mission

--- Game/DomRoute/MissionScenes/Mission3/DomM3s4Shower.gd
+++ Game/DomRoute/MissionScenes/Mission3/DomM3s4Shower.gd
@@ -95,9 +95,9 @@ func _run():
 
 		saynn("You don't have a lot of time.. but you could spare a few minutes.")
 
-		addButton("Just shower", "Just spend some time together", "just_shower")
+		addButton("Gentle shower", "Spend some time together", "just_shower")
 		addButton("Finger her", "Make that kitty purr louder", "finger_her")
-		addButtonWithChecks("Take her", "Breed that slut right here and now", "breed_her", [], [[ButtonChecks.HasReachablePenis]])
+		addButtonWithChecks("Fuck her", "Breed that slut right here and now", "breed_her", [], [[ButtonChecks.HasReachablePenis]])
 	if(state == "finger_her"):
 		playAnimation(StageScene.ShoweringDuo, "rub", {pc="elizaMom", npc="pc", bodyState={naked=true, hard=true}, npcBodyState={naked=true, hard=true}})
 		saynn("You whisper into her ear while one of your hands returns back to kneading her breasts slowly.")
@@ -175,6 +175,144 @@ func _run():
 		saynn("At least something is going right.")
 
 		addButton("Continue", "See what happens next", "endthescene")
+	if(state == "just_shower"):
+		playAnimation(StageScene.ShoweringDuo, "shower", {pc="elizaMom", npc="pc", bodyState={naked=true, hard=true}, npcBodyState={naked=true, hard=true}})
+		saynn("You let your hands rest on her hips for a moment. You lean in close to her ear, your breath tickling it.")
+
+		saynn("[say=pc]Just relax. Let me take care of you.[/say]")
+
+		saynn("A soft shiver is rushing through her back, her tail brushing against your leg. You notice a certain bottle on a small ledge.")
+
+		saynn("[say=pc]Is that a soap bottle over there?[/say]")
+
+		saynn("[say=elizaMom]Best one in the whole galaxy..[/say]")
+
+		saynn("[say=pc]Smuggling contraband onto the space station, I see how it is. Hand it to me and raise your hands, nice and smooth.[/say]")
+
+		saynn("She reaches for the bottle and gives it to you. Then, slowly, she raises her arms, her fingers returning to her hair.")
+
+		saynn("You pour some of that stuff into your palms.. and then place your hands on her shoulders. She tenses for just a breath.. and then melts as you begin to knead the muscles there.")
+
+		saynn("[say=elizaMom]Mmh.. you have good hands.[/say]")
+
+		saynn("Your digits are sliding down her back, spreading the soap over the pastel fur. You trace the curve of her spine, her waist.. her hips. You try to not miss an inch.")
+
+		saynn("Her hands are busy washing her hair.. while your slide around to her front, gliding over her stomach, up her ribcage. You pour a little more soap and cup her breasts, much more gently this time. Your palms circle her heavy mounds, thumbs brushing over her hard nipples.")
+
+		saynn("[say=elizaMom]Ah..[/say]")
+
+		saynn("You squeeze softly, working the soap into her fur, making the woman shiver again.. a good sign that you should switch your focus. One hand stays on her breast while another slides down her stomach, through the wet fur and cupping her crotch, palm pressing against her slit, feeling the warmth there. The feline lets out another shaky breath.")
+
+		saynn("When all that's clean, you move lower, your digits sliding over her thighs, down her legs. You kneel behind her, working the soap into her ankles.. and rear paws. She puts one hand against the wall, steadying herself as you wash her every toe.. and all the paw pads.")
+
+		saynn("[say=elizaMom]You're.. very thorough..[/say]")
+
+		saynn("Time is running out.. but only her butt is left. You pour more soap and cup the firm cheeks, kneading them slowly, feeling the muscles beneath the soft fur. She arches her back just slightly, pressing into your touch. Little scritches around the base of her tail make her shiver erratically.. in a cute way. The fluffy tail itself gets some love too, you give it a few long strokes along its length while it tries to curl around your wrists constantly.")
+
+		saynn("After you're fully done, you embrace her again.")
+
+		saynn("[say=elizaMom]That felt so nice.. You know.. I could turn around and return the favor..[/say]")
+
+		saynn("Best if she doesn't know who you are.")
+
+		saynn("[say=pc]Maybe some other time. Right now I need to take something from your locker.[/say]")
+
+		saynn("Her ears perk.")
+
+		saynn("[say=elizaMom]That's a big ask.. Do you promise to return it?[/say]")
+
+		saynn("[say=pc]I think it will be given back to you.[/say]")
+
+		saynn("[say=elizaMom]It's not my panties, is it~?[/say]")
+
+		saynn("When she says that, you're already gone from her stall.")
+
+		addButton("Continue", "See what happens next", "after_sex_grab_badge")
+	if(state == "breed_her"):
+		playAnimation(StageScene.SearchSex, "sex", {pc="elizaMom", npc="pc", bodyState={naked=true, hard=true}, npcBodyState={naked=true, hard=true}})
+		saynn("Something tells you that she likes it rough. And so rough she shall receive, the feeling is mutual.")
+
+		saynn("Your hands slide down to her hips and pull her back against you.. hard. She gasps, her tail trapped between the two bodies.")
+
+		saynn("[say=elizaMom]Oh-![/say]")
+
+		saynn("You spread her ass with both hands, possessively, exposing her pink slit to the spray. You take a moment to look at her, water running down her folds. You slide two fingers down her sex, spreading her open and fingering her with a few quick thrusts, making sure that she is ready.")
+
+		saynn("She is.")
+
+		saynn("You don't waste another second, quickly lining yourself up and ramming your {pc.penis} inside her.")
+
+		saynn("[say=elizaMom]AH![/say]")
+
+		saynn("Her cry echoes off the tiles, her paws land onto the wall, trying to grab onto something. You don't give her time to adjust, pulling back and throwing your hips forward again, hard, burying yourself to the hilt.")
+
+		saynn("[say=elizaMom]F-fuck-.. oh god..[/say]")
+
+		saynn("Your hips slap against her ass as you establish a rhythm. Water cascades down both of you still while your hands are gripping her waist, her tits swaying with every thrust.")
+
+		saynn("[say=elizaMom]Ah.. ahh.. ah-..[/say]")
+
+		addButton("Faster", "Increase the pace", "breed_fast")
+	if(state == "breed_fast"):
+		playAnimation(StageScene.SearchSex, "fast", {pc="elizaMom", npc="pc", bodyState={naked=true, hard=true}, npcBodyState={naked=true, hard=true}})
+		saynn("You increase the pace, every thrust is spreading her slit wide, your cock hammering deep, smashing against the entrance of her womb. She is tight, hot, clenching around you each time you ram inside.")
+
+		saynn("[say=elizaMom]I'm.. I'm gonna..[/say]")
+
+		saynn("She cums first, her whole body shuddering, legs shaking, her pussy clamping down even harder around your shaft. She cries out, her claws scraping lines into the tiles.")
+
+		var pcHasKnot = GM.pc.bodypartHasTrait(BodypartSlot.Penis, PartTrait.PenisKnot)
+		saynn("You don't stop. You keep fucking her through her orgasm, pounding into her oversensitive walls. The orgasm makes her legs weaker so you slap her ass to help the bloodflow."+str(" Your cock is also receiving extra blood actually.. your knot has been inflating more and more.." if pcHasKnot else "")+"")
+
+		saynn("[say=elizaMom]Ah.. f-fuck..[/say]")
+
+		addButton("Cum inside", "Breed the bitch", "just_breed_cum")
+		addButton("Pull out", "You changed your mind!", "just_breed_pullout")
+	if(state == "just_breed_cum"):
+		playAnimation(StageScene.SearchSex, "inside", {pc="elizaMom", npc="pc", npcCum=true, bodyState={naked=true, hard=true}, npcBodyState={naked=true, hard=true}})
+		var pcHasKnot = GM.pc.bodypartHasTrait(BodypartSlot.Penis, PartTrait.PenisKnot)
+		if (pcHasKnot):
+			saynn("You pull her back one last time and shove your cock deep. The knot slams against the woman's pussy entrance, stretching them wide.")
+
+			saynn("[say=elizaMom]W-wait.. what are you-.. AH![/say]")
+
+			saynn("She doesn't get to finish. With a wet pop, the knot slides inside her, making her pussy look impossibly stretched a second.. before you two get locked together. She cries out again, her back arching, claws scratching at the tiles. Her inner walls clamp down on the knot.. as the orgasm washes over you.")
+
+			saynn("Hot seed pumps into her womb, rope after rope, hot and thick, flooding her insides. She gasps again, letting her body milk your balls dry.")
+
+			saynn("[say=elizaMom]Too b-big.. f-fuck.. I feel it.. inside me..[/say]")
+
+			saynn("Her belly has received a nice bump from all of this. Her legs are shaking, body trembling around you. You stay there, waiting a few long seconds..")
+
+			saynn("And then.. with a sharp yank.. you pull the knot free. A gush of your {pc.cum} follows, spilling from her gaping pussy, dripping down her thighs. She slumps against the wall, her legs giving out, water still landing on her.")
+
+		else:
+			saynn("You pull her back one last time and shove your cock balls deep, burying yourself to the base. Your hips lock against her ass.. as the orgasm washes over you.")
+
+			saynn("Hot seed pumps into her womb, rope after rope. She gasps, her body twitching, inner walls milking your balls dry. You stay there for a moment, letting every last drop fill her.")
+
+			saynn("[say=elizaMom]S-so much.. f-fuck.. I feel it.. inside me..[/say]")
+
+			saynn("Then you pull out. A gush of {pc.cum} follows.. spilling from her used pussy, dripping down her thighs. She slumps against the wall, her legs giving out, water still landing on her.")
+
+		saynn("Her breathing comes in short, erratic gasps.")
+
+		saynn("There is no time aftercare.. so you let the water clean your cock.. before you step out.")
+
+		addButton("Continue", "See what happens next", "after_sex_grab_badge")
+	if(state == "just_breed_pullout"):
+		playAnimation(StageScene.SearchSex, "tease", {pc="elizaMom", npc="pc", npcCum=true, bodyState={naked=true, hard=true}, npcBodyState={naked=true, hard=true}})
+		saynn("At the last possible second, you decide to change your mind and pull out. Your throbbing cock flips out with a wet sound.. followed by hot ropes of thick seed landing across the woman's ass, one after another, marking her fur.. until the water starts washing it off.")
+
+		saynn("She is left panting heavily, her body twitching, pussy still twitching from the aftershocks.")
+
+		saynn("[say=elizaMom]Ahh.. hah..[/say]")
+
+		saynn("She slumps against the wall, her legs finally giving out.")
+
+		saynn("There is no time aftercare.. so you let the water clean your cock.. before you step out.")
+
+		addButton("Continue", "See what happens next", "after_sex_grab_badge")
 
 func _react(_action: String, _args):
 	if(_action == "endthescene"):
@@ -182,10 +320,10 @@ func _react(_action: String, _args):
 		return
 
 	if(_action == "just_grab_badge"):
-		addMessage("Task updated! Go back to Kait and Avy.")
+		addMessage("Task updated!")
 
 	if(_action == "leave_after_saw"):
-		addMessage("Task updated! Go back to Kait and Avy.")
+		addMessage("Task updated!")
 
 	if(_action == "embrace_scarlet"):
 		GM.pc.afterTakingAShower()
@@ -200,6 +338,16 @@ func _react(_action: String, _args):
 		processTime(5*60)
 
 	if(_action == "after_sex_grab_badge"):
-		addMessage("Task updated! Go back to Kait and Avy.")
+		addMessage("Task updated!")
+
+	if(_action == "just_breed_cum"):
+		processTime(3*60)
+		getCharacter("elizaMom").cummedInVaginaBy("pc")
+		GM.pc.orgasmFrom("elizaMom")
+
+	if(_action == "just_breed_pullout"):
+		processTime(3*60)
+		getCharacter("elizaMom").cummedOnBy("pc", FluidSource.Penis)
+		GM.pc.orgasmFrom("elizaMom")
 
 	setState(_action)
